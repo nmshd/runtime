1278badf830957eb08c66513e38dc3faa1f18cb2
mkuhn <magnus.kuhn@js-soft.com>
2024-02-15 11:39:01 +0100

    fix: failing tests
    apply the renaming to the tests

--- packages/consumption/test/modules/attributes/AttributesController.test.ts
+++ packages/consumption/test/modules/attributes/AttributesController.test.ts
@@ -603,7 +603,7 @@ describe("AttributesController", function () {
 
                 const validationResult = await consumptionController.attributes.validateAttributeSuccessionCommon(predecessor.id, successorData);
                 expect(validationResult).errorValidationResult({
-                    code: "error.consumption.attributes.invalidPredecessor"
+                    code: "error.consumption.attributes.differentPredecessorNamedInSuccessor"
                 });
             });
 
@@ -866,7 +866,7 @@ describe("AttributesController", function () {
 
                 const validationResult = await consumptionController.attributes.validateOwnSharedIdentityAttributeSuccession(predecessor.id, successorParams);
                 expect(validationResult).errorValidationResult({
-                    code: "error.consumption.attributes.invalidSuccessionOfOwnSharedIdentityAttribute"
+                    code: "error.consumption.attributes.successorSourceDoesNotSucceedPredecessorSource"
                 });
             });
         });
