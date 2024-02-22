3c95c94cb320d0d4ae98e3dccf7aee1c9a70dab1
Magnus Kuhn <127854942+Magnus-Kuhn@users.noreply.github.com>
2024-02-22 14:50:48 +0100

    Refactor/disable timeout for all runtime tests while debugging (#33)
    * refactor: disable timeout for all runtime tests while debugging
    
    * refactor: create own jestSettings file
    
    * refactor: rename jestSettings -> disableTimeoutForDebugging

--- packages/runtime/test/consumption/attributes-e2e.test.ts
+++ packages/runtime/test/consumption/attributes-e2e.test.ts
@@ -30,11 +30,6 @@ import {
     waitForRecipientToReceiveNotification
 } from "../lib";
 
-/* Disable timeout errors if we're debugging */
-if (process.env.NODE_OPTIONS !== undefined && process.env.NODE_OPTIONS.search("inspect") !== -1) {
-    jest.setTimeout(1e9);
-}
-
 const runtimeServiceProvider = new RuntimeServiceProvider();
 
 let services1: TestRuntimeServices;
