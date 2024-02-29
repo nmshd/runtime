a0d9a2e77e1b981500bf60235b22d8bab9ce7848
Julian König <julian.koenig@js-soft.com>
2024-02-29 11:35:46 +0100

    refactor: make toISO usage typesafe

--- packages/transport/src/core/types/CoreDate.ts
+++ packages/transport/src/core/types/CoreDate.ts
@@ -134,13 +134,11 @@ export class CoreDate extends CoreSerializable {
      * Creates an ISO String.
      */
     public override toString(): string {
-        if (!this.dateTime.isValid) throw new TransportError("The date is invalid.");
-        return this.dateTime.toISO()!;
+        return this.asValidDateTime.toISO();
     }
 
     public toISOString(): string {
-        if (!this.dateTime.isValid) throw new TransportError("The date is invalid.");
-        return this.dateTime.toISO()!;
+        return this.asValidDateTime.toISO();
     }
 
     public override toLocaleString(): string {
@@ -148,13 +146,16 @@ export class CoreDate extends CoreSerializable {
     }
 
     public override toJSON(): string {
-        if (!this.dateTime.isValid) throw new TransportError("The date is invalid.");
-        return this.dateTime.toISO()!;
+        return this.asValidDateTime.toISO();
     }
 
     public override serialize(): string {
+        return this.asValidDateTime.toISO();
+    }
+
+    private get asValidDateTime(): DateTime<true> {
         if (!this.dateTime.isValid) throw new TransportError("The date is invalid.");
-        return this.dateTime.toISO()!;
+        return this.dateTime as DateTime<true>;
     }
 
     protected static override preFrom(value: any): any {
