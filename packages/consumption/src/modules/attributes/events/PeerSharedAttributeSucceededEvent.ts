f1925411855d14a077b71b822a0bd916dd11d7e3
Julian König <julian.koenig@js-soft.com>
2024-03-25 16:22:51 +0100

    fix: update event namespaces

--- packages/consumption/src/modules/attributes/events/PeerSharedAttributeSucceededEvent.ts
+++ packages/consumption/src/modules/attributes/events/PeerSharedAttributeSucceededEvent.ts
@@ -3,7 +3,7 @@ import { LocalAttribute } from "../local/LocalAttribute";
 import { AttributeSucceededEventData } from "./AttributeSucceededEventData";
 
 export class PeerSharedAttributeSucceededEvent extends TransportDataEvent<AttributeSucceededEventData> {
-    public static readonly namespace = "consumption.peerSharedAttributeSucceded";
+    public static readonly namespace = "consumption.peerSharedAttributeSucceeded";
 
     public constructor(eventTargetAddress: string, predecessor: LocalAttribute, successor: LocalAttribute) {
         super(PeerSharedAttributeSucceededEvent.namespace, eventTargetAddress, { predecessor, successor });
