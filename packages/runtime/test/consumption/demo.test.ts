b7d1150f8e80c843005b166e3857a78b4ee06900
mkuhn <magnus.kuhn@js-soft.com>
2024-03-26 21:26:57 +0100

    feat: add wildcard/path parameters to demo

--- packages/runtime/test/consumption/demo.test.ts
+++ packages/runtime/test/consumption/demo.test.ts
@@ -7,17 +7,18 @@ test("Simple mocking example", async () => {
     server.listen({ onUnhandledRequest: "bypass" });
 
     server.use(
-        http.post("https://test.de/test", async ({ request }) => {
+        http.post("https://test.de/test/:id/*", async ({ request, params }) => {
+            // *: wildcard ; :id declares id as a path parameter
             const requestJson = await new Response(request.body).json();
-            return HttpResponse.json({ ...requestJson, secondName: "Maverick" });
+            return HttpResponse.json({ ...requestJson, lastName: "Maverick", id: params.id }); // uses the declared id as path parameter
         })
     );
 
     const instance = axios.create({
         baseURL: "https://test.de"
     });
-    const response = await instance.post("/test", {
+    const response = await instance.post("/test/id1/stuff", {
         firstName: "Fred"
     });
-    expect(response.data).toStrictEqual({ firstName: "Fred", lastName: "Maverick" });
+    expect(response.data).toStrictEqual({ firstName: "Fred", lastName: "Maverick", id: "id1" });
 });
