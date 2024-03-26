cda6a04b50c2a1fc7f9fc560bb358313c6dd7d46
mkuhn <magnus.kuhn@js-soft.com>
2024-03-26 21:12:45 +0100

    msw type support should in theory look like this

--- packages/runtime/test/consumption/demo.test.ts
+++ packages/runtime/test/consumption/demo.test.ts
@@ -2,17 +2,17 @@ import axios from "axios";
 import { http, HttpResponse } from "msw";
 import { setupServer, SetupServerApi } from "msw/node";
 
-interface Name {
+type Name = {
     firstName: string;
-    secondName: string;
-}
+    lastName: string;
+};
 
 test("Simple mocking example", async () => {
     const server: SetupServerApi = setupServer();
     server.listen({ onUnhandledRequest: "bypass" });
 
     server.use(
-        http.post<any, any, Name>("https://test.de/test", async ({ request }) => {
+        http.post<any, any, Name, any>("https://test.de/test", async ({ request }) => {
             const requestJson = await new Response(request.body).json();
             return HttpResponse.json({ ...requestJson, secondName: "Maverick" });
         })
@@ -24,5 +24,5 @@ test("Simple mocking example", async () => {
     const response = await instance.post("/test", {
         firstName: "Fred"
     });
-    expect(response.data).toStrictEqual({ firstName: "Fred", secondName: "Maverick" } as Name);
+    expect(response.data).toStrictEqual({ firstName: "Fred", lastName: "Maverick" } as Name);
 });
