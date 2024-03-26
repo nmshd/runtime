211de84ad81d947d8e657409f42b76e7b9f2af60
mkuhn <magnus.kuhn@js-soft.com>
2024-03-26 21:14:27 +0100

    refactor: remove msw return type

--- packages/runtime/test/consumption/demo.test.ts
+++ packages/runtime/test/consumption/demo.test.ts
@@ -2,17 +2,12 @@ import axios from "axios";
 import { http, HttpResponse } from "msw";
 import { setupServer, SetupServerApi } from "msw/node";
 
-type Name = {
-    firstName: string;
-    lastName: string;
-};
-
 test("Simple mocking example", async () => {
     const server: SetupServerApi = setupServer();
     server.listen({ onUnhandledRequest: "bypass" });
 
     server.use(
-        http.post<any, any, Name, any>("https://test.de/test", async ({ request }) => {
+        http.post("https://test.de/test", async ({ request }) => {
             const requestJson = await new Response(request.body).json();
             return HttpResponse.json({ ...requestJson, secondName: "Maverick" });
         })
@@ -24,5 +19,5 @@ test("Simple mocking example", async () => {
     const response = await instance.post("/test", {
         firstName: "Fred"
     });
-    expect(response.data).toStrictEqual({ firstName: "Fred", lastName: "Maverick" } as Name);
+    expect(response.data).toStrictEqual({ firstName: "Fred", lastName: "Maverick" });
 });
