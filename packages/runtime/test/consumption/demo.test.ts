30901a8a9989247e5506e46a70508b89c406eb2f
mkuhn <magnus.kuhn@js-soft.com>
2024-03-27 08:57:18 +0100

    feat: add response body type

--- packages/runtime/test/consumption/demo.test.ts
+++ packages/runtime/test/consumption/demo.test.ts
@@ -5,14 +5,26 @@ import { setupServer, SetupServerApi } from "msw/node";
 test("Simple mocking example", async () => {
     const server: SetupServerApi = setupServer();
     server.listen({ onUnhandledRequest: "bypass" });
-
+    interface Name {
+        firstName: string;
+        lastName: string;
+        id: string;
+    }
     server.use(
-        http.post("https://test.de/test/:id/*", async ({ request, params }) => {
+        http.post<any, any, Name, any>("https://test.de/test/:id/*", async ({ request, params }) => {
             // *: wildcard ; :id declares id as a path parameter
             const requestJson = await new Response(request.body).json();
             return HttpResponse.json({ ...requestJson, lastName: "Maverick", id: params.id }); // uses the declared id as path parameter
         })
     );
+
+    // the following code would not compile due to the wrong type
+
+    // server.use(
+    //     http.post<any, any, Name, any>("https://test.de/test", () => {
+    //         return HttpResponse.json({ lastName: "name" });
+    //     })
+    // );
 
     const instance = axios.create({
         baseURL: "https://test.de"
@@ -20,5 +32,7 @@ test("Simple mocking example", async () => {
     const response = await instance.post("/test/id1/stuff", {
         firstName: "Fred"
     });
+
+    server.close();
     expect(response.data).toStrictEqual({ firstName: "Fred", lastName: "Maverick", id: "id1" });
 });
