36daefa2fc844ac2f7dc631a154317cbf89c4a26
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-04-05 13:53:24 +0200

    Feature/unregister push tokens (#87)
    * feat: support unregistering push notifications
    
    * chore: version bumps
    
    * chore: test naming

---
+++ packages/runtime/src/useCases/transport/account/UnregisterPushNotificationToken.ts
@@ -0,0 +1,16 @@
+import { Result } from "@js-soft/ts-utils";
+import { AccountController } from "@nmshd/transport";
+import { Inject } from "typescript-ioc";
+import { UseCase } from "../../common";
+
+export class UnregisterPushNotificationTokenUseCase extends UseCase<void, void> {
+    public constructor(@Inject private readonly accountController: AccountController) {
+        super();
+    }
+
+    protected async executeInternal(): Promise<Result<void>> {
+        await this.accountController.unregisterPushNotificationToken();
+
+        return Result.ok(undefined);
+    }
+}
