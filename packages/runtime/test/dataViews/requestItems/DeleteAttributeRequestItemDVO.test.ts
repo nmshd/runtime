0ed8809ce4cae557cf2fe96fe5d1ac30bfebd8e9
Milena Czierlinski <milena.czierlinski@js-soft.com>
2024-04-11 17:41:22 +0200

    fix: check if list contains result

--- packages/runtime/test/dataViews/requestItems/DeleteAttributeRequestItemDVO.test.ts
+++ packages/runtime/test/dataViews/requestItems/DeleteAttributeRequestItemDVO.test.ts
@@ -139,7 +139,7 @@ describe("DeleteAttributeRequestItemDVO", () => {
         expect(dvo.request).toBeDefined();
         expect(dvo.request.isOwn).toBe(false);
         expect(["DecisionRequired", "ManualDecisionRequired"]).toContain(dvo.request.status);
-        expect(["i18n://dvo.localRequest.status.DecisionRequired", "i18n://dvo.localRequest.status.ManualDecisionRequired"]).toBe(dvo.request.statusText);
+        expect(["i18n://dvo.localRequest.status.DecisionRequired", "i18n://dvo.localRequest.status.ManualDecisionRequired"]).toContain(dvo.request.statusText);
         expect(dvo.request.type).toBe("LocalRequestDVO");
         expect(dvo.request.content.type).toBe("RequestDVO");
         expect(dvo.request.content.items).toHaveLength(1);
