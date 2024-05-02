6300552526824f3503b1ee33a9077205757fd113
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-05-02 12:19:58 +0200

    chore: remove interface implementation

--- packages/transport/src/modules/sync/data/ExternalEvent.ts
+++ packages/transport/src/modules/sync/data/ExternalEvent.ts
@@ -1,26 +1,27 @@
 import { Serializable, serialize, validate } from "@js-soft/ts-serval";
-import { BackboneExternalEvent } from "../backbone/BackboneExternalEvent";
 
-export class ExternalEvent extends Serializable implements BackboneExternalEvent {
+export class ExternalEvent extends Serializable {
     @serialize()
     @validate()
     public id: string;
+
     @serialize()
     @validate()
     public type: string;
+
     @serialize()
     @validate()
     public index: number;
+
     @serialize()
     @validate()
     public createdAt: string;
+
     @serialize()
     @validate()
     public syncErrorCount: number;
+
     @serialize()
     @validate()
     public payload: object;
-    public static from(value: BackboneExternalEvent): ExternalEvent {
-        return this.fromAny(value);
-    }
 }
