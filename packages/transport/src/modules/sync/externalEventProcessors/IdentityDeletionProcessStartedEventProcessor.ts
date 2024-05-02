a75df490b8a69be520468618a14dda1201ee6def
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-05-02 14:24:58 +0200

    chore: add external event payload deserialization

--- packages/transport/src/modules/sync/externalEventProcessors/IdentityDeletionProcessStartedEventProcessor.ts
+++ packages/transport/src/modules/sync/externalEventProcessors/IdentityDeletionProcessStartedEventProcessor.ts
@@ -1,11 +1,18 @@
+import { Serializable, serialize, validate } from "@js-soft/ts-serval";
 import { IdentityDeletionProcessStatusChangedEvent } from "../../../events";
 import { IdentityDeletionProcess } from "../../accounts/data/IdentityDeletionProcess";
 import { BackboneExternalEvent } from "../backbone/BackboneExternalEvent";
 import { ExternalEventProcessor } from "./ExternalEventProcessor";
 
+class IdentityDeletionProcessStartedEventData extends Serializable {
+    @serialize()
+    @validate()
+    public deletionProcessId: string;
+}
+
 export class IdentityDeletionProcessStartedEventProcessor extends ExternalEventProcessor {
     public override async execute(externalEvent: BackboneExternalEvent): Promise<IdentityDeletionProcess> {
-        const messageReceivedPayload = externalEvent.payload as { deletionProcessId: string };
+        const messageReceivedPayload = IdentityDeletionProcessStartedEventData.fromAny(externalEvent.payload);
         const identityDeletionProcessJSONResponse = await this.accountController.identityDeletionProcess.identityDeletionProcessClient.getIdentityDeletionProcess(
             messageReceivedPayload.deletionProcessId
         );
