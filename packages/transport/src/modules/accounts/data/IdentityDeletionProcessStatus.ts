7c5572e5de83b1b1befb9e60ed5f135d6edd323c
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2024-05-02 10:31:18 +0200

    chore: remove todos

--- packages/transport/src/modules/accounts/data/IdentityDeletionProcessStatus.ts
+++ packages/transport/src/modules/accounts/data/IdentityDeletionProcessStatus.ts
@@ -1,13 +1,7 @@
 export enum IdentityDeletionProcessStatus {
-    // TODO: Timo: rename 'WaitingForApproval' zu 'Pending'.
-    //       - wäre konsistent mit Benennung der Usecases.
     WaitingForApproval = "WaitingForApproval",
     Rejected = "Rejected",
     Approved = "Approved",
     Deleting = "Deleting",
     Cancelled = "Cancelled"
 }
-
-// TODO: Idee: Finaler Terminierungsstatus
-//       - müsste manuell durch Runtime gesetzt werden
-//       - wäre für App mglw. nützlich zur Anzeige
