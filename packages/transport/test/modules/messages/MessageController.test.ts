55601fbdbcfe48b83486a658c1772980400d2d95
mkuhn <magnus.kuhn@js-soft.com>
2024-05-21 14:56:50 +0200

    fix: error message in message controller test

--- packages/transport/test/modules/messages/MessageController.test.ts
+++ packages/transport/test/modules/messages/MessageController.test.ts
@@ -182,6 +182,6 @@ describe("MessageController", function () {
 
     test("should not send a message on a terminated relationship", async function () {
         await TestUtil.terminateRelationship(sender, recipient);
-        await expect(TestUtil.sendMessage(sender, recipient)).rejects.toThrow("error.transport.messages.noMatchingRelationship");
+        await expect(TestUtil.sendMessage(sender, recipient)).rejects.toThrow("error.transport.messages.missingOrInactiveRelationship");
     });
 });
