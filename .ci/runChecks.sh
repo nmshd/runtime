621735cbcbbedf5d86e30397f0e387b1f5306310
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-07-03 12:13:14 +0200

    chore: bump dependencies

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -2,7 +2,7 @@ set -e
 
 npm ci
 npm run build:node
-npm run lint:eslint
+npm run lint:eslint -- --rule 'jest/no-conditional-in-test: off'
 npm run lint:prettier
 npx -ws license-check
 npx better-npm-audit audit --exclude 1096302
