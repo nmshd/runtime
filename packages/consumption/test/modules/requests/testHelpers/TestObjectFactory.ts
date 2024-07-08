4c5136fb61cfb1a648e683905b451c348237bb01
mkuhn <magnus.kuhn@js-soft.com>
2024-07-08 13:54:37 +0200

    Revert "fix: relationshipstatus test utils"
    This reverts commit 30c735434e8e9f5854b53502859d6a3d49f60f54.

--- packages/consumption/test/modules/requests/testHelpers/TestObjectFactory.ts
+++ packages/consumption/test/modules/requests/testHelpers/TestObjectFactory.ts
@@ -19,7 +19,6 @@ import {
 } from "@nmshd/content";
 import { CoreBuffer, CryptoCipher, CryptoEncryptionAlgorithm, CryptoExchangeAlgorithm, CryptoSecretKey, CryptoSignatureAlgorithm, CryptoSignaturePublicKey } from "@nmshd/crypto";
 import {
-    BackboneRelationshipStatus,
     CachedRelationship,
     CoreAddress,
     CoreDate,
@@ -32,6 +31,7 @@ import {
     Message,
     Relationship,
     RelationshipAuditLogEntryReason,
+    RelationshipStatus,
     RelationshipTemplate,
     RelationshipTemplatePublicKey
 } from "@nmshd/transport";
@@ -51,7 +51,7 @@ export class TestObjectFactory {
                         publicKey: CoreBuffer.from("L1sPFQgS5CxgGs1ejBcWCQLCpeFXbRc1TQnSpuHQqDQ")
                     })
                 }),
-            status: properties?.status ?? BackboneRelationshipStatus.Pending,
+            status: properties?.status ?? RelationshipStatus.Pending,
             relationshipSecretId: properties?.relationshipSecretId ?? CoreId.from("RELSEC1"),
             cachedAt: properties?.cachedAt ?? CoreDate.from("2020-01-02T00:00:00.000Z"),
             cache:
@@ -63,7 +63,7 @@ export class TestObjectFactory {
                             createdBy: CoreAddress.from("id2"),
                             createdByDevice: CoreId.from("DVC1"),
                             reason: RelationshipAuditLogEntryReason.Creation,
-                            newStatus: BackboneRelationshipStatus.Pending
+                            newStatus: RelationshipStatus.Pending
                         }
                     ],
                     template: this.createIncomingRelationshipTemplate()
@@ -83,7 +83,7 @@ export class TestObjectFactory {
                         publicKey: CoreBuffer.from("L1sPFQgS5CxgGs1ejBcWCQLCpeFXbRc1TQnSpuHQqDQ")
                     })
                 }),
-            status: properties?.status ?? BackboneRelationshipStatus.Active,
+            status: properties?.status ?? RelationshipStatus.Active,
             relationshipSecretId: properties?.relationshipSecretId ?? CoreId.from("RELSEC1"),
             cachedAt: properties?.cachedAt ?? CoreDate.from("2020-01-02T00:00:00.000Z"),
             cache:
@@ -95,7 +95,7 @@ export class TestObjectFactory {
                             createdBy: CoreAddress.from("id2"),
                             createdByDevice: CoreId.from("DVC1"),
                             reason: RelationshipAuditLogEntryReason.Creation,
-                            newStatus: BackboneRelationshipStatus.Pending
+                            newStatus: RelationshipStatus.Pending
                         },
 
                         {
@@ -103,8 +103,8 @@ export class TestObjectFactory {
                             createdBy: CoreAddress.from("did:e:a-domain:dids:anidentity"),
                             createdByDevice: CoreId.from("DVC1"),
                             reason: RelationshipAuditLogEntryReason.AcceptanceOfCreation,
-                            oldStatus: BackboneRelationshipStatus.Pending,
-                            newStatus: BackboneRelationshipStatus.Active
+                            oldStatus: RelationshipStatus.Pending,
+                            newStatus: RelationshipStatus.Active
                         }
                     ],
                     template: this.createIncomingRelationshipTemplate()
@@ -124,7 +124,7 @@ export class TestObjectFactory {
                         publicKey: CoreBuffer.from("L1sPFQgS5CxgGs1ejBcWCQLCpeFXbRc1TQnSpuHQqDQ")
                     })
                 }),
-            status: properties?.status ?? BackboneRelationshipStatus.Terminated,
+            status: properties?.status ?? RelationshipStatus.Terminated,
             relationshipSecretId: properties?.relationshipSecretId ?? CoreId.from("RELSEC1"),
             cachedAt: properties?.cachedAt ?? CoreDate.from("2020-01-02T00:00:00.000Z"),
             cache:
@@ -136,7 +136,7 @@ export class TestObjectFactory {
                             createdBy: CoreAddress.from("id2"),
                             createdByDevice: CoreId.from("DVC1"),
                             reason: RelationshipAuditLogEntryReason.Creation,
-                            newStatus: BackboneRelationshipStatus.Pending
+                            newStatus: RelationshipStatus.Pending
                         },
 
                         {
@@ -144,8 +144,8 @@ export class TestObjectFactory {
                             createdBy: CoreAddress.from("did:e:a-domain:dids:anidentity"),
                             createdByDevice: CoreId.from("DVC1"),
                             reason: RelationshipAuditLogEntryReason.AcceptanceOfCreation,
-                            oldStatus: BackboneRelationshipStatus.Pending,
-                            newStatus: BackboneRelationshipStatus.Active
+                            oldStatus: RelationshipStatus.Pending,
+                            newStatus: RelationshipStatus.Active
                         },
 
                         {
@@ -153,8 +153,8 @@ export class TestObjectFactory {
                             createdBy: CoreAddress.from("did:e:a-domain:dids:anidentity"),
                             createdByDevice: CoreId.from("DVC1"),
                             reason: RelationshipAuditLogEntryReason.Termination,
-                            oldStatus: BackboneRelationshipStatus.Active,
-                            newStatus: BackboneRelationshipStatus.Terminated
+                            oldStatus: RelationshipStatus.Active,
+                            newStatus: RelationshipStatus.Terminated
                         }
                     ],
                     template: this.createIncomingRelationshipTemplate()
--- packages/transport/test/testHelpers/TestUtil.ts
+++ packages/transport/test/testHelpers/TestUtil.ts
@@ -12,7 +12,6 @@ import { DurationLike } from "luxon";
 import { LogLevel } from "typescript-logging";
 import {
     AccountController,
-    BackboneRelationshipStatus,
     ChangedItems,
     CoreAddress,
     CoreDate,
@@ -22,10 +21,11 @@ import {
     File,
     IChangedItems,
     IConfigOverwrite,
-    ISendFileParameters,
     IdentityDeletionProcess,
+    ISendFileParameters,
     Message,
     Relationship,
+    RelationshipStatus,
     RelationshipTemplate,
     RequestError,
     TokenContentRelationshipTemplate,
@@ -327,16 +327,16 @@ export class TestUtil {
         const syncedRelationships = await TestUtil.syncUntilHasRelationships(from);
         expect(syncedRelationships).toHaveLength(1);
         const pendingRelationship = syncedRelationships[0];
-        expect(pendingRelationship.status).toStrictEqual(BackboneRelationshipStatus.Pending);
+        expect(pendingRelationship.status).toStrictEqual(RelationshipStatus.Pending);
 
         const rejectedRelationshipFromSelf = await from.relationships.reject(pendingRelationship.id);
-        expect(rejectedRelationshipFromSelf.status).toStrictEqual(BackboneRelationshipStatus.Rejected);
+        expect(rejectedRelationshipFromSelf.status).toStrictEqual(RelationshipStatus.Rejected);
 
         // Get accepted relationship
         const syncedRelationshipsPeer = await TestUtil.syncUntilHasRelationships(to);
         expect(syncedRelationshipsPeer).toHaveLength(1);
         const acceptedRelationshipPeer = syncedRelationshipsPeer[0];
-        expect(acceptedRelationshipPeer.status).toStrictEqual(BackboneRelationshipStatus.Rejected);
+        expect(acceptedRelationshipPeer.status).toStrictEqual(RelationshipStatus.Rejected);
     }
 
     public static async addRelationship(
@@ -364,10 +364,10 @@ export class TestUtil {
         const syncedRelationships = await TestUtil.syncUntilHasRelationships(from);
         expect(syncedRelationships).toHaveLength(1);
         const pendingRelationship = syncedRelationships[0];
-        expect(pendingRelationship.status).toStrictEqual(BackboneRelationshipStatus.Pending);
+        expect(pendingRelationship.status).toStrictEqual(RelationshipStatus.Pending);
 
         const acceptedRelationshipFromSelf = await from.relationships.accept(pendingRelationship.id);
-        expect(acceptedRelationshipFromSelf.status).toStrictEqual(BackboneRelationshipStatus.Active);
+        expect(acceptedRelationshipFromSelf.status).toStrictEqual(RelationshipStatus.Active);
 
         // Get accepted relationship
         await this.sleep(300);
@@ -377,7 +377,7 @@ export class TestUtil {
 
         expect(syncedRelationshipsPeer).toHaveLength(1);
         const acceptedRelationshipPeer = syncedRelationshipsPeer[0];
-        expect(acceptedRelationshipPeer.status).toStrictEqual(BackboneRelationshipStatus.Active);
+        expect(acceptedRelationshipPeer.status).toStrictEqual(RelationshipStatus.Active);
         expect(relRequest.id.toString()).toBe(acceptedRelationshipFromSelf.id.toString());
         expect(relRequest.id.toString()).toBe(acceptedRelationshipPeer.id.toString());
 
