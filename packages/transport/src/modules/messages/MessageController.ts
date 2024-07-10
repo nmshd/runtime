cfd2661b6502508ebbd9a2a96b93eb455200d6fe
mkuhn <magnus.kuhn@js-soft.com>
2024-07-10 14:43:55 +0200

    fix: missing await

--- packages/transport/src/modules/messages/MessageController.ts
+++ packages/transport/src/modules/messages/MessageController.ts
@@ -65,7 +65,7 @@ export class MessageController extends TransportController {
 
     public async cleanupMessagesOfDecomposedRelationship(relationship: Relationship): Promise<void> {
         const messages = await this.getMessagesByRelationshipId(relationship.id);
-        messages.forEach((message) => this.cleanupMessageOfDecomposedRelationship(message.id, relationship));
+        messages.forEach(async (message) => await this.cleanupMessageOfDecomposedRelationship(message.id, relationship));
     }
 
     private async cleanupMessageOfDecomposedRelationship(messageId: CoreId, relationship: Relationship): Promise<void> {
