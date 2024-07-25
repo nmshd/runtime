e3366c5cc63a391217d6bff7b5c776832bb55f9f
mkuhn <magnus.kuhn@js-soft.com>
2024-07-25 12:39:49 +0200

    fix: wait for event

--- packages/runtime/test/dataViews/IdentityDVO.test.ts
+++ packages/runtime/test/dataViews/IdentityDVO.test.ts
@@ -1,6 +1,6 @@
 import { LocalRequestStatus } from "@nmshd/consumption";
 import { ShareAttributeRequestItemJSON } from "@nmshd/content";
-import { IncomingRequestStatusChangedEvent, PeerRelationshipTemplateDVO, RelationshipTemplateDTO } from "../../src";
+import { IncomingRequestStatusChangedEvent, LocalRequestDTO, PeerRelationshipTemplateDVO, RelationshipTemplateDTO } from "../../src";
 import { createTemplate, RuntimeServiceProvider, TestRuntimeServices } from "../lib";
 
 const serviceProvider = new RuntimeServiceProvider();
@@ -8,6 +8,7 @@ let templator: TestRuntimeServices;
 let requestor: TestRuntimeServices;
 let templatorTemplate: RelationshipTemplateDTO;
 let requestorTemplate: RelationshipTemplateDTO;
+let request: LocalRequestDTO;
 
 beforeAll(async () => {
     const runtimeServices = await serviceProvider.launch(2, { enableRequestModule: true });
@@ -44,6 +45,8 @@ describe("IdentityDVO after loading a relationship template sharing a DisplayNam
         };
         templatorTemplate = await createTemplate(templator.transport, templateContent);
         requestorTemplate = (await requestor.transport.relationshipTemplates.loadPeerRelationshipTemplate({ reference: templatorTemplate.truncatedReference })).value;
+        const requestEvent = await requestor.eventBus.waitForEvent(IncomingRequestStatusChangedEvent, (e) => e.data.newStatus === LocalRequestStatus.DecisionRequired);
+        request = requestEvent.data.request;
     });
 
     test("IdentityDVO should use the DisplayName when expanding a relationship template", async () => {
@@ -54,9 +57,6 @@ describe("IdentityDVO after loading a relationship template sharing a DisplayNam
     });
 
     test("IdentityDVO should use the DisplayName when expanding a request", async () => {
-        const requestEvent = await requestor.eventBus.waitForEvent(IncomingRequestStatusChangedEvent, (e) => e.data.newStatus === LocalRequestStatus.DecisionRequired);
-        const request = (await requestor.consumption.incomingRequests.getRequest({ id: requestEvent.data.request.id })).value;
-
         const dvo = await requestor.expander.expandLocalRequestDTO(request);
         expect(dvo).toBeDefined();
         expect(dvo.createdBy.name).toBe("A Display Name");
