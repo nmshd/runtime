481cfb82379c94d19bb79ea2a78347f449fc2e74
Julian König <julian.koenig@js-soft.com>
2024-08-19 12:41:06 +0200

    ci: publish using tags

---
+++ .ci/publishNpm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$VERSION" ]; then
+    echo "The environment variable 'VERSION' must be set."
+    exit 1
+fi
+
+# set the version of all packages in the workspace to $VERSION
+npm version -ws $VERSION
+
+# replace all dependencies from the current workspace that are marked with "*" with the version that gets published
+find . -regex "./packages/[A-Za-z-]*/package\.json$" -exec sed -i -e "s/"\*"/"$VERSION"/g" {} \;
+
+# publish all packages
+npm exec -ws -c 'enhanced-publish --if-possible --use-preid-as-tag'
