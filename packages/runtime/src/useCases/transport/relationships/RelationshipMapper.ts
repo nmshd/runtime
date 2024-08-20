2026682ff5dd11ca3ba2a72be24cb88cdfcc2baa
mkuhn <magnus.kuhn@js-soft.com>
2024-08-20 13:06:50 +0200

    refactor: review comment

--- packages/runtime/src/useCases/transport/relationships/RelationshipMapper.ts
+++ packages/runtime/src/useCases/transport/relationships/RelationshipMapper.ts
@@ -16,11 +16,11 @@ export class RelationshipMapper {
             template: RelationshipTemplateMapper.toRelationshipTemplateDTO(relationship.cache.template),
             status: relationship.status,
             peer: relationship.peer.address.toString(),
-            peerDeletionInfo: !relationship.peerDeletionInfo
-                ? undefined
-                : {
+            peerDeletionInfo: relationship.peerDeletionInfo
+                ? {
                       deletionStatus: relationship.peerDeletionInfo.deletionStatus
-                  },
+                  }
+                : undefined,
             peerIdentity: {
                 address: relationship.peer.address.toString(),
                 publicKey: relationship.peer.publicKey.toBase64(false)
