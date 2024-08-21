4818241706a58fa21f7ab18af656e9f680d1e3de
mkuhn <magnus.kuhn@js-soft.com>
2024-08-21 09:23:31 +0200

    test: adapt tests

--- packages/transport/test/modules/account/AccountController.test.ts
+++ packages/transport/test/modules/account/AccountController.test.ts
@@ -1,5 +1,5 @@
 import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
-import { AccountController, Transport } from "../../../src";
+import { AccountController, Transport, VersionController } from "../../../src";
 import { TestUtil } from "../../testHelpers/TestUtil";
 
 describe("AccountController", function () {
@@ -31,32 +31,23 @@ describe("AccountController", function () {
     });
 
     test("should correctly check a valid backbone version", async function () {
-        const transport = TestUtil.createTransport(connection, { supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 50 });
-        await transport.init();
-        const accounts = await TestUtil.provideAccounts(transport, 1);
-        account = accounts[0];
+        const versionController = new VersionController({ ...transport.config, supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 5000 });
 
-        const versionCheckResult = await account.validateUsedBackboneVersion();
+        const versionCheckResult = await versionController.checkBackboneCompatibility();
         expect(versionCheckResult.isSuccess).toBe(true);
     });
 
     test("should catch a too low backbone version", async function () {
-        const transport = TestUtil.createTransport(connection, { supportedMinBackboneVersion: 5000, supportedMaxBackboneVersion: 6000 });
-        await transport.init();
-        const accounts = await TestUtil.provideAccounts(transport, 1);
-        account = accounts[0];
+        const versionController = new VersionController({ ...transport.config, supportedMinBackboneVersion: 5000, supportedMaxBackboneVersion: 6000 });
 
-        const versionCheckResult = await account.validateUsedBackboneVersion();
+        const versionCheckResult = await versionController.checkBackboneCompatibility();
         expect(versionCheckResult.error.code).toBe("error.transport.files.runtimeVersionIncompatibleWithBackboneVersion");
     });
 
     test("should catch a too high backbone version", async function () {
-        const transport = TestUtil.createTransport(connection, { supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 2 });
-        await transport.init();
-        const accounts = await TestUtil.provideAccounts(transport, 1);
-        account = accounts[0];
+        const versionController = new VersionController({ ...transport.config, supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 2 });
 
-        const versionCheckResult = await account.validateUsedBackboneVersion();
+        const versionCheckResult = await versionController.checkBackboneCompatibility();
         expect(versionCheckResult.error.code).toBe("error.transport.files.runtimeVersionIncompatibleWithBackboneVersion");
     });
 });
