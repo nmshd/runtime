d3034399bc10696ebc514680f59c543f65327586
mkuhn <magnus.kuhn@js-soft.com>
2024-08-22 14:40:34 +0200

    refactor: separate test file

--- packages/transport/test/modules/account/AccountController.test.ts
+++ packages/transport/test/modules/account/AccountController.test.ts
@@ -1,5 +1,5 @@
 import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
-import { AccountController, Transport, VersionController } from "../../../src";
+import { AccountController, Transport } from "../../../src";
 import { TestUtil } from "../../testHelpers/TestUtil";
 
 describe("AccountController", function () {
@@ -28,26 +28,5 @@ describe("AccountController", function () {
     // eslint-disable-next-line jest/expect-expect
     test("should init a second time", async function () {
         await account!.init();
-    });
-
-    test("should correctly check a valid backbone version", async function () {
-        const versionController = new VersionController({ ...transport.config, supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 5000 });
-
-        const versionCheckResult = await versionController.checkBackboneCompatibility();
-        expect(versionCheckResult.isSuccess).toBe(true);
-    });
-
-    test("should catch a too low backbone version", async function () {
-        const versionController = new VersionController({ ...transport.config, supportedMinBackboneVersion: 5000, supportedMaxBackboneVersion: 6000 });
-
-        const versionCheckResult = await versionController.checkBackboneCompatibility();
-        expect(versionCheckResult.error.code).toBe("error.transport.files.runtimeVersionIncompatibleWithBackboneVersion");
-    });
-
-    test("should catch a too high backbone version", async function () {
-        const versionController = new VersionController({ ...transport.config, supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 2 });
-
-        const versionCheckResult = await versionController.checkBackboneCompatibility();
-        expect(versionCheckResult.error.code).toBe("error.transport.files.runtimeVersionIncompatibleWithBackboneVersion");
     });
 });
---
+++ packages/transport/test/modules/backboneCompatibility/BackboneCompatibilityController.test.ts
@@ -0,0 +1,41 @@
+import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
+import { BackboneCompatibilityController, Transport } from "src";
+import { TestUtil } from "test/testHelpers/TestUtil";
+
+describe("AccountController", function () {
+    let connection: IDatabaseConnection;
+
+    let transport: Transport;
+
+    beforeAll(async function () {
+        connection = await TestUtil.createDatabaseConnection();
+        transport = TestUtil.createTransport(connection);
+
+        await transport.init();
+    });
+
+    afterAll(async function () {
+        await connection.close();
+    });
+
+    test("should correctly check a valid backbone version", async function () {
+        const versionController = new BackboneCompatibilityController({ ...transport.config, supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 5000 });
+
+        const versionCheckResult = await versionController.checkBackboneCompatibility();
+        expect(versionCheckResult.isSuccess).toBe(true);
+    });
+
+    test("should catch a too low backbone version", async function () {
+        const versionController = new BackboneCompatibilityController({ ...transport.config, supportedMinBackboneVersion: 5000, supportedMaxBackboneVersion: 6000 });
+
+        const versionCheckResult = await versionController.checkBackboneCompatibility();
+        expect(versionCheckResult.error.code).toBe("error.transport.files.runtimeVersionIncompatibleWithBackboneVersion");
+    });
+
+    test("should catch a too high backbone version", async function () {
+        const versionController = new BackboneCompatibilityController({ ...transport.config, supportedMinBackboneVersion: 1, supportedMaxBackboneVersion: 2 });
+
+        const versionCheckResult = await versionController.checkBackboneCompatibility();
+        expect(versionCheckResult.error.code).toBe("error.transport.files.runtimeVersionIncompatibleWithBackboneVersion");
+    });
+});
