d7266acb6212852d5ec2cd07865b618008cf90e1
Milena Czierlinski <milena.czierlinski@js-soft.com>
2024-08-22 11:00:32 +0200

    chore: undo index changes

--- packages/consumption/src/consumption/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./ConsumptionBaseController";
-export * from "./ConsumptionConfig";
-export * from "./ConsumptionController";
-export * from "./ConsumptionControllerName";
-export * from "./ConsumptionError";
-export * from "./ConsumptionIds";
-export * from "./CoreErrors";
