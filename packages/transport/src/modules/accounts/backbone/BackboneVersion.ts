22b921546e2dbb8624fe9dfd88c09a92aa227c57
Julian König <julian.koenig@js-soft.com>
2024-08-26 10:09:32 +0200

    refactor: move to own module

--- packages/transport/src/modules/accounts/backbone/BackboneVersion.ts
+++
@@ -1,3 +0,0 @@
-export interface BackboneVersion {
-    majorVersion: number;
-}
--- packages/transport/src/modules/accounts/backbone/VersionClient.ts
+++
@@ -1,9 +0,0 @@
-import { RESTClient } from "../../../core";
-import { ClientResult } from "../../../core/backbone/ClientResult";
-import { BackboneVersion } from "./BackboneVersion";
-
-export class VersionClient extends RESTClient {
-    public async getBackboneVersion(): Promise<ClientResult<BackboneVersion>> {
-        return await this.get<BackboneVersion>("/api/v1/Version");
-    }
-}
