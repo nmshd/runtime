26b39a5142e996c1ff34fbbc297f52caeb657b16
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-09-10 15:15:11 +0200

    chore: add newline

--- packages/runtime/src/modules/DeciderModule.ts
+++ packages/runtime/src/modules/DeciderModule.ts
@@ -48,6 +48,7 @@ export class DeciderModule extends RuntimeModule {
     ) {
         const request = event.data.request;
         const requestId = request.id;
+
         switch (request.source!.type) {
             case "RelationshipTemplate":
                 const getTemplateResult = await services.transportServices.relationshipTemplates.getRelationshipTemplate({ id: request.source!.reference });
