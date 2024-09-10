0c9237ea39c214137a065ba8b9ebfec543ba6a22
Milena Czierlinski <146972016+Milena-Czierlinski@users.noreply.github.com>
2024-09-10 15:44:25 +0200

    Simplify Decider module (#266)
    * refactor: simplify publishEvent function
    
    * refactor: use general runtimeServices in tests
    
    * refactor: add variable requestId
    
    * chore: add newline
    
    ---------
    
    Co-authored-by: Julian König <33655937+jkoenig134@users.noreply.github.com>

--- packages/runtime/test/modules/DeciderModule.test.ts
+++ packages/runtime/test/modules/DeciderModule.test.ts
@@ -1,77 +1,72 @@
 import { RelationshipTemplateContent, Request } from "@nmshd/content";
 import { CoreDate } from "@nmshd/core-types";
 import {
-    ConsumptionServices,
     IncomingRequestStatusChangedEvent,
     LocalRequestStatus,
     MessageProcessedEvent,
     MessageProcessedResult,
     RelationshipTemplateProcessedEvent,
-    RelationshipTemplateProcessedResult,
-    TransportServices
+    RelationshipTemplateProcessedResult
 } from "../../src";
-import { MockEventBus, RuntimeServiceProvider, TestRequestItem, establishRelationship, exchangeMessage } from "../lib";
+import { RuntimeServiceProvider, TestRequestItem, TestRuntimeServices, establishRelationship, exchangeMessage } from "../lib";
 
 const runtimeServiceProvider = new RuntimeServiceProvider();
-let sTransportServices: TransportServices;
-let rConsumptionServices: ConsumptionServices;
-let rEventBus: MockEventBus;
-let rTransportServices: TransportServices;
+
+let sender: TestRuntimeServices;
+let recipient: TestRuntimeServices;
 
 beforeAll(async () => {
     const runtimeServices = await runtimeServiceProvider.launch(2, { enableDeciderModule: true });
-    sTransportServices = runtimeServices[0].transport;
 
-    rConsumptionServices = runtimeServices[1].consumption;
-    rEventBus = runtimeServices[1].eventBus;
-    rTransportServices = runtimeServices[1].transport;
+    sender = runtimeServices[0];
+    recipient = runtimeServices[1];
 
-    await establishRelationship(sTransportServices, rTransportServices);
+    await establishRelationship(sender.transport, recipient.transport);
 }, 30000);
 
 beforeEach(function () {
-    rEventBus.reset();
+    recipient.eventBus.reset();
 });
 
 afterAll(async () => await runtimeServiceProvider.stop());
 
 describe("DeciderModule", () => {
     test("moves an incoming Request from a Message into status 'ManualDecisionRequired' after it reached status 'DecisionRequired'", async () => {
-        const message = await exchangeMessage(sTransportServices, rTransportServices);
+        const message = await exchangeMessage(sender.transport, recipient.transport);
 
-        const receivedRequestResult = await rConsumptionServices.incomingRequests.received({
+        const receivedRequestResult = await recipient.consumption.incomingRequests.received({
             receivedRequest: { "@type": "Request", items: [{ "@type": "TestRequestItem", mustBeAccepted: false }] },
             requestSourceId: message.id
         });
 
-        await rConsumptionServices.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
+        await recipient.consumption.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
 
-        await expect(rEventBus).toHavePublished(
+        await expect(recipient.eventBus).toHavePublished(
             IncomingRequestStatusChangedEvent,
             (e) => e.data.newStatus === LocalRequestStatus.ManualDecisionRequired && e.data.request.id === receivedRequestResult.value.id
         );
 
-        const requestAfterAction = await rConsumptionServices.incomingRequests.getRequest({ id: receivedRequestResult.value.id });
+        const requestAfterAction = await recipient.consumption.incomingRequests.getRequest({ id: receivedRequestResult.value.id });
         expect(requestAfterAction.value.status).toStrictEqual(LocalRequestStatus.ManualDecisionRequired);
     });
 
     test("triggers MessageProcessedEvent", async () => {
-        const message = await exchangeMessage(sTransportServices, rTransportServices);
+        const message = await exchangeMessage(sender.transport, recipient.transport);
 
-        const receivedRequestResult = await rConsumptionServices.incomingRequests.received({
+        const receivedRequestResult = await recipient.consumption.incomingRequests.received({
             receivedRequest: { "@type": "Request", items: [{ "@type": "TestRequestItem", mustBeAccepted: false }] },
             requestSourceId: message.id
         });
 
-        await rConsumptionServices.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
+        await recipient.consumption.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
 
-        await expect(rEventBus).toHavePublished(MessageProcessedEvent, (e) => e.data.result === MessageProcessedResult.ManualRequestDecisionRequired);
+        await expect(recipient.eventBus).toHavePublished(MessageProcessedEvent, (e) => e.data.result === MessageProcessedResult.ManualRequestDecisionRequired);
     });
 
     test("moves an incoming Request from a Relationship Template into status 'ManualDecisionRequired' after it reached status 'DecisionRequired'", async () => {
         const request = Request.from({ items: [TestRequestItem.from({ mustBeAccepted: false })] });
         const template = (
-            await sTransportServices.relationshipTemplates.createOwnRelationshipTemplate({
+            await sender.transport.relationshipTemplates.createOwnRelationshipTemplate({
                 content: RelationshipTemplateContent.from({
                     onNewRelationship: request
                 }).toJSON(),
@@ -79,21 +74,21 @@ describe("DeciderModule", () => {
             })
         ).value;
 
-        await rTransportServices.relationshipTemplates.loadPeerRelationshipTemplate({ reference: template.truncatedReference });
+        await recipient.transport.relationshipTemplates.loadPeerRelationshipTemplate({ reference: template.truncatedReference });
 
-        const receivedRequestResult = await rConsumptionServices.incomingRequests.received({
+        const receivedRequestResult = await recipient.consumption.incomingRequests.received({
             receivedRequest: request.toJSON(),
             requestSourceId: template.id
         });
 
-        await rConsumptionServices.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
+        await recipient.consumption.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
 
-        await expect(rEventBus).toHavePublished(
+        await expect(recipient.eventBus).toHavePublished(
             IncomingRequestStatusChangedEvent,
             (e) => e.data.newStatus === LocalRequestStatus.ManualDecisionRequired && e.data.request.id === receivedRequestResult.value.id
         );
 
-        const requestAfterAction = await rConsumptionServices.incomingRequests.getRequest({ id: receivedRequestResult.value.id });
+        const requestAfterAction = await recipient.consumption.incomingRequests.getRequest({ id: receivedRequestResult.value.id });
 
         expect(requestAfterAction.value.status).toStrictEqual(LocalRequestStatus.ManualDecisionRequired);
     });
@@ -101,7 +96,7 @@ describe("DeciderModule", () => {
     test("triggers RelationshipTemplateProcessedEvent for an incoming Request from a Template after it reached status 'DecisionRequired'", async () => {
         const request = Request.from({ items: [TestRequestItem.from({ mustBeAccepted: false })] });
         const template = (
-            await sTransportServices.relationshipTemplates.createOwnRelationshipTemplate({
+            await sender.transport.relationshipTemplates.createOwnRelationshipTemplate({
                 content: RelationshipTemplateContent.from({
                     onNewRelationship: request
                 }).toJSON(),
@@ -109,21 +104,21 @@ describe("DeciderModule", () => {
             })
         ).value;
 
-        await rTransportServices.relationshipTemplates.loadPeerRelationshipTemplate({ reference: template.truncatedReference });
+        await recipient.transport.relationshipTemplates.loadPeerRelationshipTemplate({ reference: template.truncatedReference });
 
-        const receivedRequestResult = await rConsumptionServices.incomingRequests.received({
+        const receivedRequestResult = await recipient.consumption.incomingRequests.received({
             receivedRequest: request.toJSON(),
             requestSourceId: template.id
         });
 
-        await rConsumptionServices.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
+        await recipient.consumption.incomingRequests.checkPrerequisites({ requestId: receivedRequestResult.value.id });
 
-        await rEventBus.waitForEvent(
+        await recipient.eventBus.waitForEvent(
             IncomingRequestStatusChangedEvent,
             (e) => e.data.newStatus === LocalRequestStatus.DecisionRequired && e.data.request.id === receivedRequestResult.value.id
         );
 
-        await expect(rEventBus).toHavePublished(
+        await expect(recipient.eventBus).toHavePublished(
             RelationshipTemplateProcessedEvent,
             (e) => e.data.template.id === template.id && e.data.result === RelationshipTemplateProcessedResult.ManualRequestDecisionRequired
         );
