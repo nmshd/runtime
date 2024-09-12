009744a5ba15b4695ad1277dd7c13b74d133f3e8
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-09-12 09:53:15 +0200

    test: cleanup tests

--- packages/transport/test/core/backbone/CorrelationId.test.ts
+++ packages/transport/test/core/backbone/CorrelationId.test.ts
@@ -1,29 +1,33 @@
+import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
 import correlationIdLib from "correlation-id";
+import { AccountController } from "../../../src";
 import { RequestInterceptor } from "../../testHelpers/RequestInterceptor";
 import { TestUtil } from "../../testHelpers/TestUtil";
 
 describe("CorrelationId", function () {
-    test("should send correlation id to the backend when given", async function () {
-        // Arrange
-        const connection = await TestUtil.createDatabaseConnection();
+    let connection: IDatabaseConnection;
+    let testAccount: AccountController;
+    let interceptor: RequestInterceptor;
+    beforeAll(async function () {
+        connection = await TestUtil.createDatabaseConnection();
         const transport = TestUtil.createTransport(connection);
         await transport.init();
         const accounts = await TestUtil.provideAccounts(transport, 1);
-        const testAccount = accounts[0];
-        const interceptor = new RequestInterceptor((testAccount as any).synchronization.client);
+        testAccount = accounts[0];
+        interceptor = new RequestInterceptor((testAccount as any).synchronization.client);
+    });
 
-        // Act
+    afterAll(async function () {
+        await testAccount.close();
+        await connection.close();
+    });
+    test("should send correlation id to the backend when given", async function () {
         interceptor.start();
         await correlationIdLib.withId("test-correlation-id", async () => {
             await testAccount.syncEverything();
         });
 
-        // Assert
         const requests = interceptor.stop().requests;
         expect(requests.at(-1)!.headers!["x-correlation-id"]).toBe("test-correlation-id");
-
-        // Cleanup
-        await testAccount.close();
-        await connection.close();
     });
 });
