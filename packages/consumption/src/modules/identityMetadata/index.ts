4a0f98a606289b96e183903e7ad69ff86ca462ac
Julian König <julian.koenig@js-soft.com>
2024-09-12 12:33:33 +0200

    fix: remove old imports

--- packages/consumption/src/modules/identityMetadata/index.ts
+++ packages/consumption/src/modules/identityMetadata/index.ts
@@ -1,5 +1,3 @@
 export * from "./IdentityMetadataController";
-export * from "./local/CreateIdentityMetadataParams";
-export * from "./local/DeleteIdentityMetadataParams";
 export * from "./local/IdentityMetadata";
 export * from "./local/UpsertIdentityMetadataParams";
