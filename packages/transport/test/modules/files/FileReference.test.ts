1ce993f5ca19c64fb48bbfb2488bfdb9351ee6d0
mkuhn <magnus.kuhn@js-soft.com>
2024-09-24 10:59:53 +0200

    test: remove redundant reference tests

--- packages/transport/test/modules/files/FileReference.test.ts
+++ packages/transport/test/modules/files/FileReference.test.ts
@@ -25,26 +25,6 @@ describe("FileReference", function () {
         expect(deserialized.id.toString()).toStrictEqual(reference.id.toString());
     });
 
-    test("should serialize and deserialize correctly (no type information)", async function () {
-        const reference = FileReference.from({
-            key: await CryptoEncryption.generateKey(),
-            id: await BackboneIds.file.generateUnsafe()
-        });
-        expect(reference).toBeInstanceOf(Serializable);
-        expect(reference).toBeInstanceOf(FileReference);
-        expect(reference.key).toBeInstanceOf(CryptoSecretKey);
-        expect(reference.id).toBeInstanceOf(CoreId);
-        const serialized = reference.serialize();
-        expect(typeof serialized).toBe("string");
-        const deserialized = FileReference.deserialize(serialized);
-        expect(deserialized).toBeInstanceOf(Serializable);
-        expect(deserialized).toBeInstanceOf(FileReference);
-        expect(deserialized.key).toBeInstanceOf(CryptoSecretKey);
-        expect(deserialized.id).toBeInstanceOf(CoreId);
-        expect(deserialized.key.toBase64()).toStrictEqual(reference.key.toBase64());
-        expect(deserialized.id.toString()).toStrictEqual(reference.id.toString());
-    });
-
     test("should serialize and deserialize correctly (from unknown type)", async function () {
         const reference = FileReference.from({
             key: await CryptoEncryption.generateKey(),
--- packages/transport/test/modules/tokens/TokenReference.test.ts
+++ packages/transport/test/modules/tokens/TokenReference.test.ts
@@ -25,26 +25,6 @@ describe("TokenReference", function () {
         expect(deserialized.id.toString()).toStrictEqual(reference.id.toString());
     });
 
-    test("should serialize and deserialize correctly (no type information)", async function () {
-        const reference = TokenReference.from({
-            key: await CryptoEncryption.generateKey(),
-            id: await BackboneIds.token.generateUnsafe()
-        });
-        expect(reference).toBeInstanceOf(Serializable);
-        expect(reference).toBeInstanceOf(TokenReference);
-        expect(reference.key).toBeInstanceOf(CryptoSecretKey);
-        expect(reference.id).toBeInstanceOf(CoreId);
-        const serialized = reference.serialize();
-        expect(typeof serialized).toBe("string");
-        const deserialized = TokenReference.deserialize(serialized);
-        expect(deserialized).toBeInstanceOf(Serializable);
-        expect(deserialized).toBeInstanceOf(TokenReference);
-        expect(deserialized.key).toBeInstanceOf(CryptoSecretKey);
-        expect(deserialized.id).toBeInstanceOf(CoreId);
-        expect(deserialized.key.toBase64()).toStrictEqual(reference.key.toBase64());
-        expect(deserialized.id.toString()).toStrictEqual(reference.id.toString());
-    });
-
     test("should serialize and deserialize correctly (from unknown type)", async function () {
         const reference = TokenReference.from({
             key: await CryptoEncryption.generateKey(),
