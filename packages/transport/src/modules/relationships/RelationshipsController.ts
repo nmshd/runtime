83371c9b7cd011f4000fda7eb70a3ddb1b3ee791
Julian König <julian.koenig@js-soft.com>
2024-10-08 11:08:59 +0200

    chore: use regex

--- packages/transport/src/modules/relationships/RelationshipsController.ts
+++ packages/transport/src/modules/relationships/RelationshipsController.ts
@@ -182,7 +182,7 @@ export class RelationshipsController extends TransportController {
                 throw TransportCoreErrors.relationships.activeIdentityDeletionProcessOfOwnerOfRelationshipTemplate();
             }
 
-            if (result.error.code.startsWith("error.platform.validation.") && result.error.code.endsWith(".relationshipToTargetAlreadyExists")) {
+            if (result.error.code.match(/^error.platform.validation.(relationship|relationshipRequest).relationshipToTargetAlreadyExists$/)) {
                 throw TransportCoreErrors.relationships.relationshipNotYetDecomposedByPeer();
             }
 
