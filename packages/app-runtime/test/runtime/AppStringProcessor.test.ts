eb360de0cd12188c690bd6a99a8089bc2d7e6641
Julian König <julian.koenig@js-soft.com>
2024-10-18 18:15:57 +0200

    chore: add test for personalized RelationshipTemplate

--- packages/app-runtime/test/runtime/AppStringProcessor.test.ts
+++ packages/app-runtime/test/runtime/AppStringProcessor.test.ts
@@ -1,9 +1,25 @@
-import { mock } from "ts-mockito";
+import { ArbitraryRelationshipTemplateContentJSON } from "@nmshd/content";
+import { CoreDate } from "@nmshd/core-types";
+import { PeerRelationshipTemplateLoadedEvent } from "@nmshd/runtime";
+import assert from "assert";
+import { anyNumber, instance, mock, reset, verify, when } from "ts-mockito";
 import { AppRuntime, IUIBridge, LocalAccountDTO, LocalAccountSession } from "../../src";
-import { TestUtil } from "../lib";
+import { MockEventBus, TestUtil } from "../lib";
 
 describe("AppStringProcessor", function () {
-    let mockUiBridge: IUIBridge;
+    const mockUiBridge: IUIBridge = mock<IUIBridge>();
+
+    when(mockUiBridge.enterPassword(anyNumber())).thenCall((passwordType: number) => {
+        switch (passwordType) {
+            case 1:
+                return "password";
+            default:
+                return "0".repeat(passwordType);
+        }
+    });
+
+    const eventBus = new MockEventBus();
+
     let runtime1: AppRuntime;
     let account: LocalAccountDTO;
     let runtime1Session: LocalAccountSession;
@@ -11,7 +27,8 @@ describe("AppStringProcessor", function () {
     let runtime2: AppRuntime;
 
     let runtime2SessionA: LocalAccountSession;
-    let runtime2SessionB: LocalAccountSession;
+
+    const templateContent: ArbitraryRelationshipTemplateContentJSON = { "@type": "ArbitraryRelationshipTemplateContent", value: "value" };
 
     beforeAll(async function () {
         runtime1 = await TestUtil.createRuntime();
@@ -20,13 +37,12 @@ describe("AppStringProcessor", function () {
         account = await runtime1.accountServices.createAccount(Math.random().toString(36).substring(7));
         runtime1Session = await runtime1.selectAccount(account.id);
 
-        mockUiBridge = mock<IUIBridge>();
-        runtime2 = await TestUtil.createRuntime(undefined, mockUiBridge);
+        runtime2 = await TestUtil.createRuntime(undefined, instance(mockUiBridge), eventBus);
         await runtime2.start();
 
         const accounts = await TestUtil.provideAccounts(runtime2, 2);
         runtime2SessionA = await runtime2.selectAccount(accounts[0].id);
-        runtime2SessionB = await runtime2.selectAccount(accounts[1].id);
+        await runtime2.selectAccount(accounts[1].id);
     });
 
     afterAll(async function () {
@@ -34,10 +50,30 @@ describe("AppStringProcessor", function () {
         await runtime2.stop();
     });
 
+    afterEach(function () {
+        reset(mockUiBridge);
+    });
+
     test("should process a URL", async function () {
         const result = await runtime1.stringProcessor.processURL("nmshd://qr#", account);
         expect(result.isError).toBeDefined();
 
         expect(result.error.code).toBe("error.appStringProcessor.truncatedReferenceInvalid");
+    });
+
+    test("should properly handle a personalized RelationshipTemplate", async function () {
+        const runtime2SessionAAddress = runtime2SessionA.account.address!;
+        assert(runtime2SessionAAddress);
+
+        const templateResult = await runtime1Session.transportServices.relationshipTemplates.createOwnRelationshipTemplate({
+            content: templateContent,
+            expiresAt: CoreDate.utc().add({ days: 1 }).toISOString(),
+            forIdentity: runtime2SessionAAddress
+        });
+
+        await runtime2.stringProcessor.processTruncatedReference(templateResult.value.truncatedReference);
+
+        verify(mockUiBridge.enterPassword(anyNumber())).never();
+        eventBus.expectLastPublishedEvent(PeerRelationshipTemplateLoadedEvent);
     });
 });
