a5be78da26f717d2239ec2340b2390f1478ea9ad
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-11-07 09:42:00 +0100

    chore: use TagListDTO

--- packages/runtime/src/extensibility/facades/transport/TagsFacade.ts
+++ packages/runtime/src/extensibility/facades/transport/TagsFacade.ts
@@ -1,12 +1,12 @@
 import { Result } from "@js-soft/ts-utils";
-import { BackboneTagList } from "@nmshd/transport";
 import { Inject } from "@nmshd/typescript-ioc";
+import { TagListDTO } from "../../../types/transport/TagDTO";
 import { GetTagsUseCase } from "../../../useCases/transport/tags/GetTags";
 
 export class TagsFacade {
     public constructor(@Inject private readonly getTagsUseCase: GetTagsUseCase) {}
 
-    public async getTags(): Promise<Result<BackboneTagList>> {
+    public async getTags(): Promise<Result<TagListDTO>> {
         return await this.getTagsUseCase.execute();
     }
 }
--- packages/runtime/src/useCases/transport/tags/GetTags.ts
+++ packages/runtime/src/useCases/transport/tags/GetTags.ts
@@ -1,17 +1,16 @@
 import { Result } from "@js-soft/ts-utils";
-import { BackboneTagList, TagController } from "@nmshd/transport";
+import { TagController } from "@nmshd/transport";
 import { Inject } from "@nmshd/typescript-ioc";
+import { TagListDTO } from "../../../types/transport/TagDTO";
 import { UseCase } from "../../common";
 import { TagMapper } from "./TagMapper";
 
-export type GetTagsUseCaseResponse = BackboneTagList;
-
-export class GetTagsUseCase extends UseCase<void, GetTagsUseCaseResponse> {
+export class GetTagsUseCase extends UseCase<void, TagListDTO> {
     public constructor(@Inject private readonly tagController: TagController) {
         super();
     }
 
-    protected async executeInternal(): Promise<Result<GetTagsUseCaseResponse>> {
+    protected async executeInternal(): Promise<Result<TagListDTO>> {
         return Result.ok(TagMapper.toTagListDTO(await this.tagController.getTags()));
     }
 }
