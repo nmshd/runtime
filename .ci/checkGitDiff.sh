e4d7eccb4677d263b5385e0f46c033cb955df7c2
Julian König <julian.koenig@js-soft.com>
2024-11-12 16:01:19 +0100

    chore: show output

--- .ci/checkGitDiff.sh
+++ .ci/checkGitDiff.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git diff-index HEAD --name-only
+
 if ! git diff-index --quiet HEAD; then
     echo 'These files changed when running the command:'
     git diff --name-only | sed 's/^/* /'
