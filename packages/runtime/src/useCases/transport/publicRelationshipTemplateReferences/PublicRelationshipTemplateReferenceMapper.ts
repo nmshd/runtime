e418b0c6dc389a5572ad4be8b48167b04db2c01e
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-11-19 10:55:07 +0100

    chore: typo

--- packages/runtime/src/useCases/transport/publicRelationshipTemplateReferences/PublicRelationshipTemplateReferenceMapper.ts
+++ packages/runtime/src/useCases/transport/publicRelationshipTemplateReferences/PublicRelationshipTemplateReferenceMapper.ts
@@ -2,7 +2,7 @@ import { PublicRelationshipTemplateReference } from "@nmshd/transport";
 import { PublicRelationshipTemplateReferenceDTO } from "../../../types/transport/PublicRelationshipTemplateReferenceDTO";
 
 export class PublicRelationshipTemplateReferenceMapper {
-    public static toPublicRelationshipTemplateReferencDTO(tagList: PublicRelationshipTemplateReference): PublicRelationshipTemplateReferenceDTO {
+    public static toPublicRelationshipTemplateReferenceDTO(tagList: PublicRelationshipTemplateReference): PublicRelationshipTemplateReferenceDTO {
         return tagList;
     }
 }
