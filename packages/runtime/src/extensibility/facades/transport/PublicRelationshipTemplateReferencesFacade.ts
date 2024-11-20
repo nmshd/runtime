9fd15c5560050af02ad1d855aa85757869942243
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-11-20 09:58:43 +0100

    chore: use barrels

--- packages/runtime/src/extensibility/facades/transport/PublicRelationshipTemplateReferencesFacade.ts
+++ packages/runtime/src/extensibility/facades/transport/PublicRelationshipTemplateReferencesFacade.ts
@@ -1,7 +1,7 @@
 import { Result } from "@js-soft/ts-utils";
 import { Inject } from "@nmshd/typescript-ioc";
-import { PublicRelationshipTemplateReferenceDTO } from "../../../types/transport/PublicRelationshipTemplateReferenceDTO";
-import { GetPublicRelationshipTemplateReferencesUseCase } from "../../../useCases/transport/publicRelationshipTemplateReferences/GetPublicRelationshipTemplateReferences";
+import { PublicRelationshipTemplateReferenceDTO } from "../../../types";
+import { GetPublicRelationshipTemplateReferencesUseCase } from "../../../useCases";
 
 export class PublicRelationshipTemplateReferencesFacade {
     public constructor(@Inject private readonly getPublicRelationshipTemplateReferencesUseCase: GetPublicRelationshipTemplateReferencesUseCase) {}
