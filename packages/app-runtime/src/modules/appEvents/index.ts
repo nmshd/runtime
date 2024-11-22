7bede00005f25f9704e5ef7c0d81998b5e79105d
Milena Czierlinski <milena.czierlinski@js-soft.com>
2024-11-22 17:55:41 +0100

    chore: remove LocalAccountDeletionDateChangedModule

--- packages/app-runtime/src/modules/appEvents/index.ts
+++ packages/app-runtime/src/modules/appEvents/index.ts
@@ -1,5 +1,4 @@
 export * from "./AppLaunchModule";
-export * from "./LocalAccountDeletionDateChangedModule";
 export * from "./MailReceivedModule";
 export * from "./OnboardingChangeReceivedModule";
 export * from "./RelationshipTemplateProcessedModule";
