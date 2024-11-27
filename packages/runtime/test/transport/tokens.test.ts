239a2b94345270227ac4d859028dcfd61829924b
mkuhn <magnus.kuhn@js-soft.com>
2024-11-27 08:20:00 +0100

    test: fix error code

--- packages/runtime/test/transport/tokens.test.ts
+++ packages/runtime/test/transport/tokens.test.ts
@@ -173,7 +173,7 @@ describe("Password-protected tokens", () => {
             ephemeral: true,
             passwordProtection: { password: "" }
         });
-        expect(createResult).toBeAnError("password must NOT have fewer than 1 characters", "error.runtime.requestDeserialization");
+        expect(createResult).toBeAnError("password must NOT have fewer than 1 characters", "error.runtime.validation.invalidPropertyValue");
     });
 
     test("validation error when creating a token with an invalid PIN", async () => {
@@ -247,7 +247,7 @@ describe("Password-protected tokens for files", () => {
             fileId,
             passwordProtection: { password: "" }
         });
-        expect(createResult).toBeAnError("password must NOT have fewer than 1 characters", "error.runtime.requestDeserialization");
+        expect(createResult).toBeAnError("password must NOT have fewer than 1 characters", "error.runtime.error.runtime.validation.invalidPropertyValue");
     });
 
     test("validation error when creating a token with an invalid PIN", async () => {
@@ -325,7 +325,7 @@ describe("Password-protected tokens for unprotected templates", () => {
             templateId,
             passwordProtection: { password: "" }
         });
-        expect(createResult).toBeAnError("password must NOT have fewer than 1 characters", "error.runtime.requestDeserialization");
+        expect(createResult).toBeAnError("password must NOT have fewer than 1 characters", "error.runtime.error.runtime.validation.invalidPropertyValue");
     });
 
     test("validation error when creating a token with an invalid PIN", async () => {
