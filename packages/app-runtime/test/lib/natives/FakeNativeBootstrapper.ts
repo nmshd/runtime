94bccd41745d7a389822b792461d7528a21ff5a2
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-12-02 18:08:53 +0100

    Simplify the PushNotificationModuleTest (#354)
    * chore: make native eventbus configurable
    
    * test: simplify PushNotificationModuleTest
    
    * fix: commit stuff

--- packages/app-runtime/test/lib/natives/FakeNativeBootstrapper.ts
+++ packages/app-runtime/test/lib/natives/FakeNativeBootstrapper.ts
@@ -1,4 +1,4 @@
-import { EventEmitter2EventBus, Result } from "@js-soft/ts-utils";
+import { EventBus, EventEmitter2EventBus, Result } from "@js-soft/ts-utils";
 import { WebLoggerFactory } from "@js-soft/web-logger";
 import { INativeBootstrapper, INativeEnvironment } from "../../../src";
 import { FakeNativeConfigAccess } from "./FakeNativeConfigAccess";
@@ -7,6 +7,8 @@ import { FakeNativeDeviceInfoAccess } from "./FakeNativeDeviceInfoAccess";
 import { FakeNativeNotificationAccess } from "./FakeNativeNotificationAccess";
 
 export class FakeNativeBootstrapper implements INativeBootstrapper {
+    public constructor(private readonly eventBus?: EventBus) {}
+
     private _nativeEnvironment: INativeEnvironment;
     public get nativeEnvironment(): INativeEnvironment {
         return this._nativeEnvironment;
@@ -24,9 +26,11 @@ export class FakeNativeBootstrapper implements INativeBootstrapper {
             configAccess: new FakeNativeConfigAccess(),
             databaseFactory: new FakeNativeDatabaseFactory(),
             deviceInfoAccess: new FakeNativeDeviceInfoAccess(),
-            eventBus: new EventEmitter2EventBus(() => {
-                // noop
-            }),
+            eventBus:
+                this.eventBus ??
+                new EventEmitter2EventBus(() => {
+                    // noop
+                }),
             loggerFactory,
             notificationAccess: new FakeNativeNotificationAccess(nativeLogger)
         };
