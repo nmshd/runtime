96d267013bd44869cac88ac9f0ce08feec94ee43
Julian König <julian.koenig@js-soft.com>
2024-12-02 12:05:10 +0100

    fix: publish event

--- packages/app-runtime/src/modules/runtimeEvents/IdentityDeletionProcessStatusChangedModule.ts
+++ packages/app-runtime/src/modules/runtimeEvents/IdentityDeletionProcessStatusChangedModule.ts
@@ -1,6 +1,8 @@
 import { CoreDate } from "@nmshd/core-types";
 import { IdentityDeletionProcessStatus, IdentityDeletionProcessStatusChangedEvent } from "@nmshd/runtime";
 import { AppRuntimeError } from "../../AppRuntimeError";
+import { LocalAccountDeletionDateChangedEvent } from "../../events";
+import { LocalAccountMapper } from "../../multiAccount/data/LocalAccountMapper";
 import { AppRuntimeModule, AppRuntimeModuleConfiguration } from "../AppRuntimeModule";
 
 export interface IdentityDeletionProcessStatusChangedModuleConfig extends AppRuntimeModuleConfiguration {}
@@ -21,21 +23,27 @@ export class IdentityDeletionProcessStatusChangedModule extends AppRuntimeModule
 
         switch (identityDeletionProcess.status) {
             case IdentityDeletionProcessStatus.Approved:
-                await this.runtime.multiAccountController.updateLocalAccountDeletionDate(event.eventTargetAddress, CoreDate.from(identityDeletionProcess.gracePeriodEndsAt!));
+                await this.updateLocalAccountDeletionDate(event.eventTargetAddress, CoreDate.from(identityDeletionProcess.gracePeriodEndsAt!));
                 break;
 
             case IdentityDeletionProcessStatus.Cancelled:
-                const account = await this.runtime.multiAccountController.getAccountByAddress(event.eventTargetAddress);
-                const previousDeletionDate = account.deletionDate;
-
-                if (!previousDeletionDate) break;
-
-                await this.runtime.multiAccountController.updateLocalAccountDeletionDate(event.eventTargetAddress, undefined);
+                await this.updateLocalAccountDeletionDate(event.eventTargetAddress, undefined);
                 break;
 
             default:
                 break;
         }
+    }
+
+    private async updateLocalAccountDeletionDate(eventTargetAddress: string, deletionDate: CoreDate | undefined) {
+        const account = await this.runtime.multiAccountController.getAccountByAddress(eventTargetAddress);
+        const previousDeletionDate = account.deletionDate;
+
+        if (!deletionDate && !previousDeletionDate) return;
+        if (deletionDate && previousDeletionDate && deletionDate.equals(previousDeletionDate)) return;
+
+        const localAccount = await this.runtime.multiAccountController.updateLocalAccountDeletionDate(eventTargetAddress, deletionDate);
+        this.runtime.eventBus.publish(new LocalAccountDeletionDateChangedEvent(eventTargetAddress, LocalAccountMapper.toLocalAccountDTO(localAccount)));
     }
 
     public override stop(): Promise<void> | void {
