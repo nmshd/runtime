36b09723e398a5a7a541f0d4ef5fb0269f3854b4
mkuhn <magnus.kuhn@js-soft.com>
2024-12-03 09:31:30 +0100

    feat: use separate tsconfig

--- packages/runtime/scripts/buildSchemas.mjs
+++ packages/runtime/scripts/buildSchemas.mjs
@@ -9,24 +9,41 @@ const config = {
 
 const schemaGenerator = tsj.createGenerator(config);
 
-const requestTypes = schemaGenerator
-    .getRootNodes()
-    .map((x) => x.symbol.escapedName)
-    .filter((x) => x.endsWith("Request"));
+const customConfig = {
+    ...config,
+    tsconfig: new URL("./tsconfig.json", import.meta.url).pathname
+};
 
-const schemaDeclarations = requestTypes
-    .map((type) => {
-        try {
-            const schema = schemaGenerator.createSchema(type);
-            return `export const ${type}: any = ${JSON.stringify(schema, undefined, 4)}`;
-        } catch (e) {
-            if (!(e instanceof tsj.NoRootTypeError)) throw e;
-        }
-    })
-    .filter((s) => s)
-    .join("\n\n");
+const customGenerator = tsj.createGenerator(customConfig);
+const customTypes = getRequestTypes(customGenerator);
+const customSchemaDeclarations = getSchemaDeclarations(customGenerator, customTypes);
+
+const requestTypes = getRequestTypes(schemaGenerator).filter((x) => !customTypes.includes(x));
+
+const schemaDeclarations = getSchemaDeclarations(schemaGenerator, requestTypes);
 
 const outputPath = new URL("../src/useCases/common/Schemas.ts", import.meta.url).pathname;
-fs.writeFile(outputPath, schemaDeclarations, (err) => {
+fs.writeFile(outputPath, schemaDeclarations + "\n\n" + customSchemaDeclarations, (err) => {
     if (err) throw err;
 });
+
+function getRequestTypes(schemaGenerator) {
+    return schemaGenerator
+        .getRootNodes()
+        .map((x) => x.symbol.escapedName)
+        .filter((x) => x.endsWith("Request"));
+}
+
+function getSchemaDeclarations(schemaGenerator, requestTypes) {
+    return requestTypes
+        .map((type) => {
+            try {
+                const schema = schemaGenerator.createSchema(type);
+                return `export const ${type}: any = ${JSON.stringify(schema, undefined, 4)}`;
+            } catch (e) {
+                if (!(e instanceof tsj.NoRootTypeError)) throw e;
+            }
+        })
+        .filter((s) => s)
+        .join("\n\n");
+}
