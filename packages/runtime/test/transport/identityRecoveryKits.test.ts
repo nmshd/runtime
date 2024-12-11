6cd777f24e04724388aa37c56e5b33ad7208fd30
Julian König <julian.koenig@js-soft.com>
2024-12-11 11:26:53 +0100

    chore: test ordering

--- packages/runtime/test/transport/identityRecoveryKits.test.ts
+++ packages/runtime/test/transport/identityRecoveryKits.test.ts
@@ -38,36 +38,6 @@ afterEach(async () => {
 });
 
 describe("Identity Recovery Kits", () => {
-    describe("errors", () => {
-        test("should not be possible to create a recovery kit when the datawallet is disabled", async () => {
-            const response = await servicesWithDisabledDatawallet.transport.identityRecoveryKits.createIdentityRecoveryKit({
-                profileName: "profileName",
-                passwordProtection: { password: "password" }
-            });
-            expect(response).toBeAnError("Datawallet is disabled. Recovery kits can only be created when datawallet is enabled.", "error.runtime.recoveryKit.datawalletDisabled");
-        });
-
-        test.each([
-            [{ password: "" }, "passwordProtection/password must NOT have fewer than 1 characters"],
-            [
-                { password: "aPassword", passwordIsPin: true },
-                "'passwordProtection.passwordIsPin' is true, hence 'passwordProtection.password' must consist of 4 to 16 digits from 0 to 9."
-            ],
-            [
-                { password: "123", passwordIsPin: true },
-                "'passwordProtection.passwordIsPin' is true, hence 'passwordProtection.password' must consist of 4 to 16 digits from 0 to 9."
-            ],
-            [
-                { password: "123456789123456789123456789123456789", passwordIsPin: true },
-                "'passwordProtection.passwordIsPin' is true, hence 'passwordProtection.password' must consist of 4 to 16 digits from 0 to 9."
-            ],
-            [{ password: 123, passwordIsPin: true }, "passwordProtection/password must be string"]
-        ])("should reject the invalid password protection '%p'", async (passwordProtection: any, errorMessage) => {
-            const response = await services.transport.identityRecoveryKits.createIdentityRecoveryKit({ profileName: "profileName", passwordProtection });
-            expect(response).toBeAnError(errorMessage, "error.runtime.validation.invalidPropertyValue");
-        });
-    });
-
     test("should create a recovery kit", async () => {
         const response = await services.transport.identityRecoveryKits.createIdentityRecoveryKit({
             profileName: "profileName",
@@ -117,5 +87,35 @@ describe("Identity Recovery Kits", () => {
         expect(response).toBeSuccessful();
 
         expect(response.value.exists).toBe(true);
+    });
+
+    describe("errors", () => {
+        test("should not be possible to create a recovery kit when the datawallet is disabled", async () => {
+            const response = await servicesWithDisabledDatawallet.transport.identityRecoveryKits.createIdentityRecoveryKit({
+                profileName: "profileName",
+                passwordProtection: { password: "password" }
+            });
+            expect(response).toBeAnError("Datawallet is disabled. Recovery kits can only be created when datawallet is enabled.", "error.runtime.recoveryKit.datawalletDisabled");
+        });
+
+        test.each([
+            [{ password: "" }, "passwordProtection/password must NOT have fewer than 1 characters"],
+            [
+                { password: "aPassword", passwordIsPin: true },
+                "'passwordProtection.passwordIsPin' is true, hence 'passwordProtection.password' must consist of 4 to 16 digits from 0 to 9."
+            ],
+            [
+                { password: "123", passwordIsPin: true },
+                "'passwordProtection.passwordIsPin' is true, hence 'passwordProtection.password' must consist of 4 to 16 digits from 0 to 9."
+            ],
+            [
+                { password: "123456789123456789123456789123456789", passwordIsPin: true },
+                "'passwordProtection.passwordIsPin' is true, hence 'passwordProtection.password' must consist of 4 to 16 digits from 0 to 9."
+            ],
+            [{ password: 123, passwordIsPin: true }, "passwordProtection/password must be string"]
+        ])("should reject the invalid password protection '%p'", async (passwordProtection: any, errorMessage) => {
+            const response = await services.transport.identityRecoveryKits.createIdentityRecoveryKit({ profileName: "profileName", passwordProtection });
+            expect(response).toBeAnError(errorMessage, "error.runtime.validation.invalidPropertyValue");
+        });
     });
 });
