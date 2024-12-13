ef05861796a50e628223ad87ab403bb50619eac1
Milena Czierlinski <milena.czierlinski@js-soft.com>
2024-12-13 17:36:53 +0100

    test: offboard account of identity with ongoing grace period

--- packages/app-runtime/test/runtime/Offboarding.test.ts
+++ packages/app-runtime/test/runtime/Offboarding.test.ts
@@ -1,3 +1,4 @@
+import { IdentityDeletionProcessStatus } from "@nmshd/runtime";
 import { AppRuntime, AppRuntimeServices } from "../../src";
 import { TestUtil } from "../lib";
 
@@ -5,6 +6,7 @@ describe("Offboarding", function () {
     let runtime: AppRuntime;
 
     let services1: AppRuntimeServices;
+    let services2: AppRuntimeServices;
     let localAccount2Id: string;
     let device2Id: string;
 
@@ -14,7 +16,9 @@ describe("Offboarding", function () {
         const configOverride = { allowMultipleAccountsWithSameAddress: true };
         runtime = await TestUtil.createRuntime(configOverride);
         await runtime.start();
+    });
 
+    beforeEach(async function () {
         const [localAccount1] = await TestUtil.provideAccounts(runtime, 1);
         services1 = await runtime.getServices(localAccount1.id);
 
@@ -25,7 +29,7 @@ describe("Offboarding", function () {
 
         const localAccount2 = await runtime.accountServices.onboardAccount(onboardingInfoResult.value);
         localAccount2Id = localAccount2.id;
-        const services2 = await runtime.getServices(localAccount2.id);
+        services2 = await runtime.getServices(localAccount2.id);
 
         await services2.transportServices.account.syncDatawallet();
         await services1.transportServices.account.syncDatawallet();
@@ -35,7 +39,35 @@ describe("Offboarding", function () {
         await runtime.stop();
     });
 
-    test("offboard Account", async function () {
+    test("offboard Account for active Identity", async function () {
+        await runtime.accountServices.offboardAccount(localAccount2Id);
+        await services1.transportServices.account.syncDatawallet();
+
+        const accounts = await runtime.accountServices.getAccounts();
+        expect(accounts).toHaveLength(1);
+
+        const devicesResult = await services1.transportServices.devices.getDevices();
+        const filteredDevice = devicesResult.value.find((d) => d.id === device2Id);
+
+        expect(filteredDevice).toBeDefined();
+        expect(filteredDevice!.isOffboarded).toBe(true);
+
+        const deviceResult = await services1.transportServices.devices.getDevice({ id: device2Id });
+        const device = deviceResult.value;
+
+        expect(device.isOffboarded).toBe(true);
+
+        await expect(runtime.getServices(localAccount2Id)).rejects.toThrow("error.transport.recordNotFound");
+        await expect(runtime.selectAccount(localAccount2Id)).rejects.toThrow("error.transport.recordNotFound");
+    });
+
+    test("offboard Account for Identity within grace period of IdentityDeletionProcess", async function () {
+        await services1.transportServices.identityDeletionProcesses.initiateIdentityDeletionProcess();
+        await services2.transportServices.account.syncDatawallet();
+
+        const identityDeletionProcessOnSecondAccount = (await services2.transportServices.identityDeletionProcesses.getActiveIdentityDeletionProcess()).value;
+        expect(identityDeletionProcessOnSecondAccount.status).toStrictEqual(IdentityDeletionProcessStatus.Approved);
+
         await runtime.accountServices.offboardAccount(localAccount2Id);
         await services1.transportServices.account.syncDatawallet();
 
