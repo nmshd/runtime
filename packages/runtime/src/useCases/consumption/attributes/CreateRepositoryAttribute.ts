a9093c7c55a84f40c164cd698b182c5575c77095
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-01-21 16:49:26 +0100

    fix: add used SchemaValidatableCreateRepositoryAttributeRequest again

--- packages/runtime/src/useCases/consumption/attributes/CreateRepositoryAttribute.ts
+++ packages/runtime/src/useCases/consumption/attributes/CreateRepositoryAttribute.ts
@@ -20,6 +20,8 @@ interface AbstractCreateRepositoryAttributeRequest<T> {
 
 export interface CreateRepositoryAttributeRequest extends AbstractCreateRepositoryAttributeRequest<AttributeValues.Identity.Json> {}
 
+export interface SchemaValidatableCreateRepositoryAttributeRequest extends AbstractCreateRepositoryAttributeRequest<unknown> {}
+
 class Validator implements IValidator<CreateRepositoryAttributeRequest> {
     public constructor(@Inject private readonly schemaRepository: SchemaRepository) {}
 
