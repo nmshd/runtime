a64b3401feb223ec3ee786fc38ed1a07cd669afa
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-01-22 14:22:13 +0100

    feat: update comment

--- packages/runtime/scripts/buildSchemas.mjs
+++ packages/runtime/scripts/buildSchemas.mjs
@@ -7,7 +7,7 @@ const runtimeConfig = {
     type: "*",
     extraTags: ["errorMessage"]
 };
-// use SchemaValidatableCreateRepositoryAttributeRequest instead of CreateRepositoryAttributeRequest
+// use SchemaValidatable(Can)CreateRepositoryAttributeRequest instead of (Can)CreateRepositoryAttributeRequest
 const useCaseSchemaDeclarations = getSchemaDeclarations(
     runtimeConfig,
     (x) => x.endsWith("Request") && x !== "CreateRepositoryAttributeRequest" && x !== "CanCreateRepositoryAttributeRequest"
