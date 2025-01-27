57e84ce2ecb5a1a465b55a976ce6223c38f141b4
Britta Stallknecht <britta.stallknecht@js-soft.com>
2025-01-27 17:13:05 +0100

    feat: cannot create Relationship without responding to Request

--- packages/runtime/src/useCases/transport/relationships/CreateRelationship.ts
+++ packages/runtime/src/useCases/transport/relationships/CreateRelationship.ts
@@ -1,6 +1,7 @@
 import { Serializable } from "@js-soft/ts-serval";
 import { Result } from "@js-soft/ts-utils";
-import { ArbitraryRelationshipCreationContent, RelationshipCreationContent } from "@nmshd/content";
+import { IncomingRequestsController, LocalRequestStatus } from "@nmshd/consumption";
+import { ArbitraryRelationshipCreationContent, ArbitraryRelationshipTemplateContent, RelationshipCreationContent, RelationshipTemplateContent } from "@nmshd/content";
 import { CoreId } from "@nmshd/core-types";
 import { AccountController, RelationshipTemplate, RelationshipTemplateController, RelationshipsController } from "@nmshd/transport";
 import { Inject } from "@nmshd/typescript-ioc";
@@ -23,6 +24,7 @@ export class CreateRelationshipUseCase extends UseCase<CreateRelationshipRequest
     public constructor(
         @Inject private readonly relationshipsController: RelationshipsController,
         @Inject private readonly relationshipTemplateController: RelationshipTemplateController,
+        @Inject private readonly incomingRequestsController: IncomingRequestsController,
         @Inject private readonly accountController: AccountController,
         @Inject validator: Validator
     ) {
@@ -36,6 +38,9 @@ export class CreateRelationshipUseCase extends UseCase<CreateRelationshipRequest
             return Result.fail(RuntimeErrors.general.recordNotFound(RelationshipTemplate));
         }
 
+        const canSendRelationship = await this.relationshipsController.canSendRelationship({ creationContent: request.creationContent, template });
+        if (!canSendRelationship.isSuccess) throw canSendRelationship.error;
+
         const transformedCreationContent = Serializable.fromUnknown(request.creationContent);
         if (!(transformedCreationContent instanceof ArbitraryRelationshipCreationContent || transformedCreationContent instanceof RelationshipCreationContent)) {
             return Result.fail(
@@ -45,10 +50,52 @@ export class CreateRelationshipUseCase extends UseCase<CreateRelationshipRequest
             );
         }
 
+        const transformedTemplateContent = Serializable.fromUnknown(template.cache?.content);
+        if (transformedCreationContent instanceof ArbitraryRelationshipCreationContent && !(transformedTemplateContent instanceof ArbitraryRelationshipTemplateContent)) {
+            return Result.fail(
+                RuntimeErrors.general.invalidPropertyValue(
+                    "The creationContent of a Relationship must be an ArbitraryRelationshipCreationContent if the content of the Relationship is an ArbitraryRelationshipTemplateContent."
+                )
+            );
+        }
+
+        if (transformedCreationContent instanceof RelationshipCreationContent) {
+            if (!(transformedTemplateContent instanceof RelationshipTemplateContent)) {
+                return Result.fail(
+                    RuntimeErrors.general.invalidPropertyValue(
+                        "The creationContent of a Relationship must be a RelationshipCreationContent if the content of the Relationship is a RelationshipTemplateContent."
+                    )
+                );
+            }
+
+            const relationshipCreationContentValidationError = await this.validateRelationshipCreationContent(request.templateId, transformedCreationContent);
+            if (relationshipCreationContentValidationError) return Result.fail(relationshipCreationContentValidationError);
+        }
+
         const sendRelationshipResult = await this.relationshipsController.sendRelationship({ template, creationContent: transformedCreationContent.toJSON() });
 
         await this.accountController.syncDatawallet();
 
         return Result.ok(RelationshipMapper.toRelationshipDTO(sendRelationshipResult));
+    }
+
+    private async validateRelationshipCreationContent(templateId: RelationshipTemplateIdString, relationshipCreationContent: RelationshipCreationContent) {
+        const acceptedIncomingRequests = await this.incomingRequestsController.getIncomingRequests({
+            query: {
+                source: { reference: templateId },
+                status: [LocalRequestStatus.Decided],
+                response: { content: { result: "Accepted" } }
+            }
+        });
+
+        if (acceptedIncomingRequests.length === 0) {
+            return RuntimeErrors.general.unknown("There is no accepted incoming Request associated with the RelationshipTemplate.");
+        }
+
+        if (JSON.stringify(acceptedIncomingRequests[0].response!.content) !== JSON.stringify(relationshipCreationContent.response)) {
+            return RuntimeErrors.general.unknown("The Response of the accepted incoming Request must be provided as the response of the RelationshipCreationContent.");
+        }
+
+        return;
     }
 }
