5414c8bef62274b5aac8dd655a045edd4ecee0ad
mkuhn <magnus.kuhn@js-soft.com>
2025-01-27 16:39:51 +0100

    fix: check regex in AbstractXML

--- packages/content/src/attributes/types/strings/AbstractXML.ts
+++ packages/content/src/attributes/types/strings/AbstractXML.ts
@@ -1,10 +1,11 @@
 import { serialize, validate } from "@js-soft/ts-serval";
 import { RenderHints, RenderHintsDataType, RenderHintsEditType, ValueHints } from "../../../attributes/hints";
+import { characterSets } from "../../constants/CharacterSets";
 import { AbstractString } from "../AbstractString";
 
 export abstract class AbstractXML extends AbstractString {
     @serialize()
-    @validate({ max: 50000 })
+    @validate({ max: 50000, regExp: characterSets.din91379DatatypeC })
     public override value: string;
 
     public static override get valueHints(): ValueHints {
