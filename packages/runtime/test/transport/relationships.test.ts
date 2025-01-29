6bb5a43fbb86b182ac35bcbb189e7ef28ad24d98
Britta Stallknecht <britta.stallknecht@js-soft.com>
2025-01-29 16:34:57 +0100

    test: should not create Relationship if Request is not accepted

--- packages/runtime/test/transport/relationships.test.ts
+++ packages/runtime/test/transport/relationships.test.ts
@@ -7,6 +7,7 @@ import {
     ReadAttributeRequestItemJSON,
     RelationshipAttributeConfidentiality,
     RelationshipCreationContent,
+    RelationshipCreationContentJSON,
     RelationshipTemplateContent,
     RelationshipTemplateContentJSON,
     ResponseItemResult,
@@ -60,11 +61,12 @@ let services3: TestRuntimeServices;
 let services4: TestRuntimeServices;
 
 beforeAll(async () => {
-    const runtimeServices = await serviceProvider.launch(4, { enableRequestModule: true, enableDeciderModule: true, enableNotificationModule: true });
+    const runtimeServices = await serviceProvider.launch(3, { enableRequestModule: true, enableDeciderModule: true, enableNotificationModule: true });
+    const runtimeServicesWithDisabledRequestModule = await serviceProvider.launch(1, { enableRequestModule: false, enableDeciderModule: true, enableNotificationModule: true });
     services1 = runtimeServices[0];
     services2 = runtimeServices[1];
     services3 = runtimeServices[2];
-    services4 = runtimeServices[3];
+    services4 = runtimeServicesWithDisabledRequestModule[0];
 }, 30000);
 
 afterEach(async () => {
@@ -87,38 +89,43 @@ describe("Can Create / Create Relationship", () => {
     let relationshipId: string;
 
     describe("tests on creationContent of Relationship", () => {
-        const relationshipCreationContent = RelationshipCreationContent.from({
-            response: {
-                "@type": "Response",
-                result: ResponseResult.Accepted,
-                requestId: "RequestId",
-                items: [
-                    ReadAttributeAcceptResponseItem.from({
-                        result: ResponseItemResult.Accepted,
-                        attributeId: CoreId.from("AttributeId"),
-                        attribute: IdentityAttribute.from({
-                            owner: CoreAddress.from("addressOfServices4"),
-                            value: GivenName.from("aGivenName")
-                        })
-                    }).toJSON()
-                ]
-            }
-        }).toJSON();
+        let relationshipCreationContent: RelationshipCreationContentJSON;
+        let relationshipTemplateContent: RelationshipTemplateContentJSON;
 
-        const relationshipTemplateContent = RelationshipTemplateContent.from({
-            onNewRelationship: {
-                "@type": "Request",
-                items: [
-                    ReadAttributeRequestItem.from({
-                        mustBeAccepted: true,
-                        query: {
-                            "@type": "IdentityAttributeQuery",
-                            valueType: "GivenName"
-                        }
-                    })
-                ]
-            }
-        }).toJSON();
+        beforeAll(() => {
+            relationshipCreationContent = RelationshipCreationContent.from({
+                response: {
+                    "@type": "Response",
+                    result: ResponseResult.Accepted,
+                    requestId: "aRequestId",
+                    items: [
+                        ReadAttributeAcceptResponseItem.from({
+                            result: ResponseItemResult.Accepted,
+                            attributeId: CoreId.from("anAttributeId"),
+                            attribute: IdentityAttribute.from({
+                                owner: CoreAddress.from(services4.address),
+                                value: GivenName.from("aGivenName")
+                            })
+                        }).toJSON()
+                    ]
+                }
+            }).toJSON();
+
+            relationshipTemplateContent = RelationshipTemplateContent.from({
+                onNewRelationship: {
+                    "@type": "Request",
+                    items: [
+                        ReadAttributeRequestItem.from({
+                            mustBeAccepted: true,
+                            query: {
+                                "@type": "IdentityAttributeQuery",
+                                valueType: "GivenName"
+                            }
+                        })
+                    ]
+                }
+            }).toJSON();
+        }, 30000);
 
         test("should not create a Relationship with a false creationContent type", async () => {
             const templateId = (await exchangeTemplate(services1.transport, services4.transport)).id;
@@ -202,6 +209,43 @@ describe("Can Create / Create Relationship", () => {
                 "The creationContent of a Relationship must be a RelationshipCreationContent if the content of the Relationship is a RelationshipTemplateContent.",
                 "error.runtime.validation.invalidPropertyValue"
             );
+        });
+
+        test("should not create a Relationship if the Request of the RelationshipTemplateContent is not accepted", async () => {
+            const templateId = (await exchangeTemplate(services1.transport, services4.transport, relationshipTemplateContent)).id;
+
+            const receivedRequest = (
+                await services4.consumption.incomingRequests.received({
+                    receivedRequest: relationshipTemplateContent.onNewRelationship,
+                    requestSourceId: templateId
+                })
+            ).value;
+
+            await services4.consumption.incomingRequests.checkPrerequisites({
+                requestId: receivedRequest.id
+            });
+
+            await services4.eventBus.waitForEvent(IncomingRequestStatusChangedEvent, (e) => e.data.newStatus === LocalRequestStatus.DecisionRequired);
+            await services4.eventBus.waitForRunningEventHandlers();
+
+            const canCreateRelationshipResponse = (
+                await services4.transport.relationships.canCreateRelationship({
+                    templateId: templateId,
+                    creationContent: relationshipCreationContent
+                })
+            ).value;
+
+            assert(!canCreateRelationshipResponse.isSuccess);
+
+            expect(canCreateRelationshipResponse.isSuccess).toBe(false);
+            expect(canCreateRelationshipResponse.message).toBe("There is no accepted incoming Request associated with the RelationshipTemplate.");
+            expect(canCreateRelationshipResponse.code).toBe("error.runtime.unknown");
+
+            const createRelationshipResponse = await services4.transport.relationships.createRelationship({
+                templateId: templateId,
+                creationContent: relationshipCreationContent
+            });
+            expect(createRelationshipResponse).toBeAnError("There is no accepted incoming Request associated with the RelationshipTemplate.", "error.runtime.unknown");
         });
     });
 
