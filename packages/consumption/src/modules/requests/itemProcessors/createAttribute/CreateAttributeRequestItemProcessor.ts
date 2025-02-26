e2cb0bf090949a5b067484b0d31cfddc12254417
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-02-26 13:24:28 +0100

    feat: add todo

--- packages/consumption/src/modules/requests/itemProcessors/createAttribute/CreateAttributeRequestItemProcessor.ts
+++ packages/consumption/src/modules/requests/itemProcessors/createAttribute/CreateAttributeRequestItemProcessor.ts
@@ -128,6 +128,8 @@ export class CreateAttributeRequestItemProcessor extends GenericRequestItemProce
         const latestSharedVersions = await this.consumptionController.attributes.getSharedVersionsOfAttribute(repositoryAttribute.id, [requestInfo.peer]);
         const latestSharedVersion = latestSharedVersions.length > 0 ? latestSharedVersions[0] : undefined;
 
+        // TODO: check if deleted by peer (not owner)
+
         if (!latestSharedVersion) {
             const newOwnSharedIdentityAttribute = await this.consumptionController.attributes.createSharedLocalAttributeCopy({
                 peer: requestInfo.peer,
