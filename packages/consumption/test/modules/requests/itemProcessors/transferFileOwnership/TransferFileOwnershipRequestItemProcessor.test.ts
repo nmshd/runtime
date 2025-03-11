a416a3ea2bec577df3dcbf3a691f48d4f180f33f
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-03-11 13:04:13 +0100

    feat: check explicitly if file is expired

--- packages/consumption/test/modules/requests/itemProcessors/transferFileOwnership/TransferFileOwnershipRequestItemProcessor.test.ts
+++ packages/consumption/test/modules/requests/itemProcessors/transferFileOwnership/TransferFileOwnershipRequestItemProcessor.test.ts
@@ -181,7 +181,7 @@ describe("TransferFileOwnershipRequestItemProcessor", function () {
             const result = await recipientProcessor.canAccept(requestItem, { accept: true }, incomingRequest);
             expect(result).errorValidationResult({
                 code: "error.consumption.requests.invalidAcceptParameters",
-                message: `You cannot accept this RequestItem, since the File with the given ID '${requestItem.fileReference.id.toString()}' could not be found.`
+                message: `You cannot accept this RequestItem since the File with the given ID '${requestItem.fileReference.id.toString()}' could not be found.`
             });
         });
 
@@ -205,7 +205,7 @@ describe("TransferFileOwnershipRequestItemProcessor", function () {
             const result = await recipientProcessor.canAccept(requestItem, { accept: true }, incomingRequest);
             expect(result).errorValidationResult({
                 code: "error.consumption.requests.invalidAcceptParameters",
-                message: `You cannot accept this RequestItem, since the File with the given fileReference '${requestItem.fileReference.id.toString()}' is already owned by you.`
+                message: `You cannot accept this RequestItem since the File with the given fileReference '${requestItem.fileReference.id.toString()}' is already owned by you.`
             });
         });
 
@@ -229,7 +229,7 @@ describe("TransferFileOwnershipRequestItemProcessor", function () {
             const result = await recipientProcessor.canAccept(requestItem, { accept: true }, incomingRequest);
             expect(result).errorValidationResult({
                 code: "error.consumption.requests.invalidAcceptParameters",
-                message: `You cannot accept this RequestItem, since the File with the given fileReference '${requestItem.fileReference.id.toString()}' is not owned by the peer.`
+                message: `You cannot accept this RequestItem since the File with the given fileReference '${requestItem.fileReference.id.toString()}' is not owned by the peer.`
             });
         });
     });
