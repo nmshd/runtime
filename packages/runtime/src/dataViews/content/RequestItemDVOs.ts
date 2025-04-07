cae0e969a260954b5ae16e64fd8a55a7add45cce
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-04-07 16:52:49 +0200

    Add optional link text to consents (#476)
    * feat: add linkText to Consent attributeValue
    
    * feat: add optional linkText property to ConsentRequestItems
    
    * chore: build:schemas
    
    * chore: naming min max
    
    * chore: add validations
    
    * chore: schemas again
    
    * chore: allow shorter
    
    * chore: 3
    
    * chore
    
    * chore: back to 3 everything else is absurd

--- packages/runtime/src/dataViews/content/RequestItemDVOs.ts
+++ packages/runtime/src/dataViews/content/RequestItemDVOs.ts
@@ -59,6 +59,7 @@ export interface ConsentRequestItemDVO extends RequestItemDVO {
     type: "ConsentRequestItemDVO";
     consent: string;
     link?: string;
+    linkDisplayText?: string;
 }
 
 export interface RegisterAttributeListenerRequestItemDVO extends RequestItemDVO {
