25902820ac5182133515f61a434c1de430066bba
mkuhn <magnus.kuhn@js-soft.com>
2025-04-08 19:56:48 +0200

    test: cleanup

--- packages/runtime/test/consumption/verifiableAttributes.test.ts
+++ packages/runtime/test/consumption/verifiableAttributes.test.ts
@@ -16,6 +16,8 @@ let issuerServices: TestRuntimeServices;
 let holderServices: TestRuntimeServices;
 let verifierServices: TestRuntimeServices;
 let mockServer: SetupServerApi;
+let unsignedAttribute: IdentityAttributeJSON;
+let readAttributeRequest: CreateOutgoingRequestRequest;
 
 let issuerListUrl: string;
 
@@ -34,9 +36,31 @@ beforeAll(async () => {
     const issuerPublicMultikey = `z${CoreBuffer.from([0xed, 0x01]).append(issuerPublicKey).toBase58()}`;
     const issuerDidKey = `did/key/${issuerPublicMultikey}`;
     issuerListUrl = `${ISSUER_LIST_BASE_URI}/${issuerDidKey}`;
+
+    unsignedAttribute = IdentityAttribute.from({
+        owner: holderServices.address,
+        value: GivenName.from({
+            value: "aGivenName"
+        }).toJSON()
+    }).toJSON();
+
+    const readAttributeRequestItem = ReadAttributeRequestItem.from({
+        mustBeAccepted: false,
+        query: {
+            "@type": "IdentityAttributeQuery",
+            valueType: "GivenName",
+            isVerified: true
+        }
+    }).toJSON();
+    readAttributeRequest = {
+        peer: holderServices.address,
+        content: {
+            items: [readAttributeRequestItem]
+        }
+    };
 }, 30000);
 
-beforeEach(() => setupMocks());
+beforeEach(() => setupMocks({}));
 
 afterEach(async () => {
     await cleanupAttributes([issuerServices, holderServices, verifierServices]);
@@ -45,13 +69,6 @@ afterEach(async () => {
 afterAll(async () => await runtimeServiceProvider.stop());
 
 test.each(Object.values(SupportedVCTypes))("issue and present a credential of type %s", async (credentialType) => {
-    const unsignedAttribute = IdentityAttribute.from({
-        owner: holderServices.address,
-        value: GivenName.from({
-            value: "aGivenName"
-        }).toJSON()
-    }).toJSON();
-
     const signingResult = await issuerServices.consumption.attributes.createCreateVerifiableAttributeRequestItem({
         content: unsignedAttribute,
         peer: holderServices.address,
@@ -63,37 +80,18 @@ test.each(Object.values(SupportedVCTypes))("issue and present a credential of ty
     const requestItem = signingResult.value.requestItem;
     expect(requestItem.attribute.proof).toBeDefined();
 
-    const request: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [requestItem]
-        }
-    };
-    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, request, [{ accept: true }]);
+    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, readAttributeRequest, [{ accept: true }]);
 
     const attributes = (await holderServices.consumption.attributes.getAttributes({})).value;
     expect(attributes).toHaveLength(3);
     expect(attributes[0].content.proof).toBeDefined();
+    expect(attributes[0].content.proof?.credentialType).toBe(credentialType);
     expect(attributes[0].shareInfo).toBeUndefined();
     expect(attributes[1].content.proof).toBeUndefined();
     expect(attributes[1].shareInfo).toBeUndefined();
     expect(attributes[2].content.proof).toBeDefined();
     expect(attributes[2].shareInfo?.peer).toBe(issuerServices.address);
 
-    const readAttributeRequestItem = ReadAttributeRequestItem.from({
-        mustBeAccepted: false,
-        query: {
-            "@type": "IdentityAttributeQuery",
-            valueType: "GivenName",
-            isVerified: true
-        }
-    }).toJSON();
-    const readAttributeRequest: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [readAttributeRequestItem]
-        }
-    };
     const readAttributeResponse: AcceptReadAttributeRequestItemParametersWithExistingAttributeJSON = {
         accept: true,
         existingAttributeId: attributes[0].id
@@ -111,13 +109,6 @@ test.each([
     [SupportedVCTypes.SdJwtVc, SupportedStatusListTypes.TokenStatusList, "text"],
     [SupportedVCTypes.W3CVC, SupportedStatusListTypes.BitstringStatusList, "json"]
 ])("issue and present a credential of type %s with %s", async (vcType, statusListType, mediaType) => {
-    const unsignedAttribute = IdentityAttribute.from({
-        owner: holderServices.address,
-        value: GivenName.from({
-            value: "aGivenName"
-        }).toJSON()
-    }).toJSON();
-
     const signingResult = await issuerServices.consumption.attributes.createCreateVerifiableAttributeRequestItem({
         content: unsignedAttribute,
         peer: holderServices.address,
@@ -132,39 +123,13 @@ test.each([
     const statusListCredential = signingResult.value.statusListCredential;
     expect(statusListCredential).toBeDefined();
 
-    setupMocks(statusListCredential, mediaType);
+    setupMocks({ statusListCredential: statusListCredential, statusListMediaType: mediaType });
 
-    const request: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [requestItem]
-        }
-    };
-    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, request, [{ accept: true }]);
+    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, readAttributeRequest, [{ accept: true }]);
 
     const attributes = (await holderServices.consumption.attributes.getAttributes({})).value;
     expect(attributes).toHaveLength(3);
-    expect(attributes[0].content.proof).toBeDefined();
-    expect(attributes[0].shareInfo).toBeUndefined();
-    expect(attributes[1].content.proof).toBeUndefined();
-    expect(attributes[1].shareInfo).toBeUndefined();
-    expect(attributes[2].content.proof).toBeDefined();
-    expect(attributes[2].shareInfo?.peer).toBe(issuerServices.address);
 
-    const readAttributeRequestItem = ReadAttributeRequestItem.from({
-        mustBeAccepted: false,
-        query: {
-            "@type": "IdentityAttributeQuery",
-            valueType: "GivenName",
-            isVerified: true
-        }
-    }).toJSON();
-    const readAttributeRequest: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [readAttributeRequestItem]
-        }
-    };
     const readAttributeResponse: AcceptReadAttributeRequestItemParametersWithExistingAttributeJSON = {
         accept: true,
         existingAttributeId: attributes[0].id
@@ -173,9 +138,7 @@ test.each([
 
     const verifierAttributes = (await verifierServices.consumption.attributes.getAttributes({})).value;
     expect(verifierAttributes).toHaveLength(1);
-    expect(verifierAttributes[0].content.proof).toBeDefined();
     expect(verifierAttributes[0].content.proof?.proofInvalid).toBeUndefined();
-    expect(verifierAttributes[0].shareInfo?.peer).toBe(holderServices.address);
 });
 
 test.each([
@@ -203,15 +166,9 @@ test.each([
     const statusListCredential = signingResult.value.statusListCredential;
     expect(statusListCredential).toBeDefined();
 
-    setupMocks(statusListCredential, mediaType);
+    setupMocks({ statusListCredential: statusListCredential, statusListMediaType: mediaType });
 
-    const request: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [requestItem]
-        }
-    };
-    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, request, [{ accept: true }]);
+    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, readAttributeRequest, [{ accept: true }]);
 
     const attributes = (await holderServices.consumption.attributes.getAttributes({})).value;
     expect(attributes).toHaveLength(3);
@@ -220,22 +177,8 @@ test.each([
     const revokedStatusCredential = revocationResult.value;
     expect(revokedStatusCredential).toBeDefined();
 
-    setupMocks(revokedStatusCredential, mediaType);
+    setupMocks({ statusListCredential: revokedStatusCredential, statusListMediaType: mediaType });
 
-    const readAttributeRequestItem = ReadAttributeRequestItem.from({
-        mustBeAccepted: false,
-        query: {
-            "@type": "IdentityAttributeQuery",
-            valueType: "GivenName",
-            isVerified: true
-        }
-    }).toJSON();
-    const readAttributeRequest: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [readAttributeRequestItem]
-        }
-    };
     const readAttributeResponse: AcceptReadAttributeRequestItemParametersWithExistingAttributeJSON = {
         accept: true,
         existingAttributeId: attributes[0].id
@@ -244,19 +187,10 @@ test.each([
 
     const verifierAttributes = (await verifierServices.consumption.attributes.getAttributes({})).value;
     expect(verifierAttributes).toHaveLength(1);
-    expect(verifierAttributes[0].content.proof).toBeDefined();
     expect(verifierAttributes[0].content.proof?.proofInvalid).toBe(true);
-    expect(verifierAttributes[0].shareInfo?.peer).toBe(holderServices.address);
 });
 
 test.each(Object.values(SupportedVCTypes))("expiration of a credential of type %s", async (credentialType) => {
-    const unsignedAttribute = IdentityAttribute.from({
-        owner: holderServices.address,
-        value: GivenName.from({
-            value: "aGivenName"
-        }).toJSON()
-    }).toJSON();
-
     const signingResult = await issuerServices.consumption.attributes.createCreateVerifiableAttributeRequestItem({
         content: unsignedAttribute,
         peer: holderServices.address,
@@ -269,37 +203,11 @@ test.each(Object.values(SupportedVCTypes))("expiration of a credential of type %
     const requestItem = signingResult.value.requestItem;
     expect(requestItem.attribute.proof).toBeDefined();
 
-    const request: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [requestItem]
-        }
-    };
-    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, request, [{ accept: true }]);
+    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, readAttributeRequest, [{ accept: true }]);
 
     const attributes = (await holderServices.consumption.attributes.getAttributes({})).value;
     expect(attributes).toHaveLength(3);
-    expect(attributes[0].content.proof).toBeDefined();
-    expect(attributes[0].shareInfo).toBeUndefined();
-    expect(attributes[1].content.proof).toBeUndefined();
-    expect(attributes[1].shareInfo).toBeUndefined();
-    expect(attributes[2].content.proof).toBeDefined();
-    expect(attributes[2].shareInfo?.peer).toBe(issuerServices.address);
 
-    const readAttributeRequestItem = ReadAttributeRequestItem.from({
-        mustBeAccepted: false,
-        query: {
-            "@type": "IdentityAttributeQuery",
-            valueType: "GivenName",
-            isVerified: true
-        }
-    }).toJSON();
-    const readAttributeRequest: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [readAttributeRequestItem]
-        }
-    };
     const readAttributeResponse: AcceptReadAttributeRequestItemParametersWithExistingAttributeJSON = {
         accept: true,
         existingAttributeId: attributes[0].id
@@ -309,19 +217,10 @@ test.each(Object.values(SupportedVCTypes))("expiration of a credential of type %
 
     const verifierAttributes = (await verifierServices.consumption.attributes.getAttributes({})).value;
     expect(verifierAttributes).toHaveLength(1);
-    expect(verifierAttributes[0].content.proof).toBeDefined();
     expect(verifierAttributes[0].content.proof?.proofInvalid).toBe(true);
-    expect(verifierAttributes[0].shareInfo?.peer).toBe(holderServices.address);
 });
 
 test.each(Object.values(SupportedVCTypes))("don't trust an issuer with credential of type %s", async (credentialType) => {
-    const unsignedAttribute = IdentityAttribute.from({
-        owner: holderServices.address,
-        value: GivenName.from({
-            value: "aGivenName"
-        }).toJSON()
-    }).toJSON();
-
     const signingResult = await issuerServices.consumption.attributes.createCreateVerifiableAttributeRequestItem({
         content: unsignedAttribute,
         peer: holderServices.address,
@@ -333,69 +232,40 @@ test.each(Object.values(SupportedVCTypes))("don't trust an issuer with credentia
     const requestItem = signingResult.value.requestItem;
     expect(requestItem.attribute.proof).toBeDefined();
 
-    const request: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [requestItem]
-        }
-    };
-    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, request, [{ accept: true }]);
+    await exchangeAndAcceptRequestByMessage(issuerServices, holderServices, readAttributeRequest, [{ accept: true }]);
 
     const attributes = (await holderServices.consumption.attributes.getAttributes({})).value;
     expect(attributes).toHaveLength(3);
-    expect(attributes[0].content.proof).toBeDefined();
-    expect(attributes[0].shareInfo).toBeUndefined();
-    expect(attributes[1].content.proof).toBeUndefined();
-    expect(attributes[1].shareInfo).toBeUndefined();
-    expect(attributes[2].content.proof).toBeDefined();
-    expect(attributes[2].shareInfo?.peer).toBe(issuerServices.address);
 
-    const readAttributeRequestItem = ReadAttributeRequestItem.from({
-        mustBeAccepted: false,
-        query: {
-            "@type": "IdentityAttributeQuery",
-            valueType: "GivenName",
-            isVerified: true
-        }
-    }).toJSON();
-    const readAttributeRequest: CreateOutgoingRequestRequest = {
-        peer: holderServices.address,
-        content: {
-            items: [readAttributeRequestItem]
-        }
-    };
     const readAttributeResponse: AcceptReadAttributeRequestItemParametersWithExistingAttributeJSON = {
         accept: true,
         existingAttributeId: attributes[0].id
     };
 
-    setupMocks(undefined, undefined, true);
+    setupMocks({ issuerUntrusted: true });
 
     await exchangeAndAcceptRequestByMessage(verifierServices, holderServices, readAttributeRequest, [readAttributeResponse]);
 
     const verifierAttributes = (await verifierServices.consumption.attributes.getAttributes({})).value;
     expect(verifierAttributes).toHaveLength(1);
-    expect(verifierAttributes[0].content.proof).toBeDefined();
     expect(verifierAttributes[0].content.proof?.proofInvalid).toBe(true);
-    expect(verifierAttributes[0].shareInfo?.peer).toBe(holderServices.address);
 });
 
-function setupMocks(statusListCredential?: any, mediaType?: string, issuerUntrusted?: boolean) {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+function setupMocks(setupParams: { statusListCredential?: any; statusListMediaType?: string; issuerUntrusted?: boolean }) {
     if (mockServer) mockServer.close();
 
     const handlers = [];
     handlers.push(
         http.get(issuerListUrl, () => {
             return new HttpResponse(null, {
-                status: issuerUntrusted ? 404 : 200
+                status: setupParams.issuerUntrusted ? 404 : 200
             });
         })
     );
-    if (statusListCredential && mediaType) {
+    if (setupParams.statusListCredential && setupParams.statusListMediaType) {
         handlers.push(
             http.get(MOCK_STATUS_LIST_URI, () => {
-                return (HttpResponse as any)[mediaType](statusListCredential);
+                return (HttpResponse as any)[setupParams.statusListMediaType!](setupParams.statusListCredential);
             })
         );
     }
