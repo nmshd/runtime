6f0d5ce0b3b97a2beb92a866ada551eaea416de0
Julian König <julian.koenig@js-soft.com>
2025-04-15 17:06:28 +0200

    refactor: big brain move

--- packages/runtime/scripts/buildSchemas.mjs
+++ packages/runtime/scripts/buildSchemas.mjs
@@ -8,10 +8,7 @@ const runtimeConfig = {
     extraTags: ["errorMessage"]
 };
 
-// use SchemaValidatable(Can)... instead of ...
-const excludedTypes = ["CreateRepositoryAttributeRequest", "CanCreateRepositoryAttributeRequest", "CreateDeviceOnboardingTokenRequest"];
-const useCaseSchemaDeclarations = getSchemaDeclarations(runtimeConfig, (x) => x.endsWith("Request") && excludedTypes.indexOf(x) === -1);
-const cleanUseCaseSchemaDeclarations = useCaseSchemaDeclarations.replaceAll("SchemaValidatable", "");
+const schemaDeclarations = getSchemaDeclarations(runtimeConfig, (x) => x.endsWith("Request"));
 
 const contentConfig = {
     ...runtimeConfig,
@@ -22,21 +19,26 @@ const attributeSchemaDeclarations = getSchemaDeclarations(contentConfig, (x) => 
 const cleanAttributeSchemaDeclarations = attributeSchemaDeclarations.replaceAll("JSON", "");
 
 const outputPath = new URL("../src/useCases/common/Schemas.ts", import.meta.url).pathname;
-fs.writeFile(outputPath, `${cleanUseCaseSchemaDeclarations}\n\n${cleanAttributeSchemaDeclarations}`, (err) => {
+fs.writeFile(outputPath, `${schemaDeclarations}\n\n${cleanAttributeSchemaDeclarations}`, (err) => {
     if (err) throw err;
 });
 
 function getSchemaDeclarations(tsconfig, filter) {
     const schemaGenerator = tsj.createGenerator(tsconfig);
+
     const types = schemaGenerator
         .getRootNodes()
         .map((x) => x.symbol.escapedName)
         .filter(filter);
-    return types
+
+    const schemaValidatables = types.filter((x) => x.startsWith("SchemaValidatable")).map((x) => x.replace("SchemaValidatable", ""));
+    const typesToGenerate = types.filter((x) => !schemaValidatables.includes(x));
+
+    return typesToGenerate
         .map((type) => {
             try {
                 const schema = schemaGenerator.createSchema(type);
-                return `export const ${type}: any = ${JSON.stringify(schema, undefined, 4)}`;
+                return `export const ${type}: any = ${JSON.stringify(schema, undefined, 4)}`.replaceAll("SchemaValidatable", "");
             } catch (e) {
                 if (!(e instanceof tsj.NoRootTypeError)) throw e;
             }
