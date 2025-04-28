fe61ad441c192d4e494a8c852f96e1a86a5d8fbd
Julian König <julian.koenig@js-soft.com>
2025-04-28 12:44:23 +0200

    chore: update variable naming

--- packages/consumption/test/modules/requests/itemProcessors/formField/FormFieldRequestItemProcessor.test.ts
+++ packages/consumption/test/modules/requests/itemProcessors/formField/FormFieldRequestItemProcessor.test.ts
@@ -1106,8 +1106,8 @@ describe("FormFieldRequestItemProcessor", function () {
 
             await processor.applyIncomingResponseItem(responseItem, requestItem, incomingRequest);
 
-            const attributesAfterGettingFreeText = await consumptionController.attributes.getLocalAttributes();
-            expect(attributesAfterGettingFreeText).toHaveLength(0);
+            const attributesAfterGettingResponse = await consumptionController.attributes.getLocalAttributes();
+            expect(attributesAfterGettingResponse).toHaveLength(0);
         });
 
         test("does not create an Attribute when getting the option selected by the recipient in the single selection form field", async function () {
