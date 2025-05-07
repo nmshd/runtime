d53319c63fc623371e23eac3c566d30c6a2b5c7b
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-05-07 10:15:13 +0200

    chore: re-build schemas

--- packages/runtime/src/useCases/common/Schemas.ts
+++ packages/runtime/src/useCases/common/Schemas.ts
@@ -17577,6 +17577,58 @@ export const NotifyPeerAboutRepositoryAttributeSuccessionRequest: any = {
     }
 }
 
+export const SetDeletionInfoOfAttributeRequest: any = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$ref": "#/definitions/SetDeletionInfoOfAttributeRequest",
+    "definitions": {
+        "SetDeletionInfoOfAttributeRequest": {
+            "type": "object",
+            "properties": {
+                "attributeId": {
+                    "type": "string"
+                },
+                "deletionInfo": {
+                    "type": "object",
+                    "properties": {
+                        "deletionStatus": {
+                            "$ref": "#/definitions/LocalAttributeDeletionStatus"
+                        },
+                        "deletionDate": {
+                            "$ref": "#/definitions/ISO8601DateTimeString"
+                        }
+                    },
+                    "required": [
+                        "deletionStatus",
+                        "deletionDate"
+                    ],
+                    "additionalProperties": false
+                }
+            },
+            "required": [
+                "attributeId",
+                "deletionInfo"
+            ],
+            "additionalProperties": false
+        },
+        "LocalAttributeDeletionStatus": {
+            "type": "string",
+            "enum": [
+                "DeletionRequestSent",
+                "DeletionRequestRejected",
+                "ToBeDeleted",
+                "ToBeDeletedByPeer",
+                "DeletedByPeer",
+                "DeletedByOwner"
+            ]
+        },
+        "ISO8601DateTimeString": {
+            "type": "string",
+            "errorMessage": "must match ISO8601 datetime format",
+            "pattern": "^([+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24:?00)([.,]\\d+(?!:))?)?(\\17[0-5]\\d([.,]\\d+)?)?([zZ]|([+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$"
+        }
+    }
+}
+
 export const ShareRepositoryAttributeRequest: any = {
     "$schema": "http://json-schema.org/draft-07/schema#",
     "$ref": "#/definitions/ShareRepositoryAttributeRequest",
@@ -22925,58 +22977,6 @@ export const LoadPeerTokenRequest: any = {
         "TokenReferenceString": {
             "type": "string",
             "pattern": "VE9L.{84}"
-        }
-    }
-}
-
-export const SetDeletionInfoOfAttributeRequest: any = {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "$ref": "#/definitions/SetDeletionInfoOfAttributeRequest",
-    "definitions": {
-        "SetDeletionInfoOfAttributeRequest": {
-            "type": "object",
-            "properties": {
-                "attributeId": {
-                    "type": "string"
-                },
-                "deletionInfo": {
-                    "type": "object",
-                    "properties": {
-                        "deletionStatus": {
-                            "$ref": "#/definitions/LocalAttributeDeletionStatus"
-                        },
-                        "deletionDate": {
-                            "$ref": "#/definitions/ISO8601DateTimeString"
-                        }
-                    },
-                    "required": [
-                        "deletionStatus",
-                        "deletionDate"
-                    ],
-                    "additionalProperties": false
-                }
-            },
-            "required": [
-                "attributeId",
-                "deletionInfo"
-            ],
-            "additionalProperties": false
-        },
-        "LocalAttributeDeletionStatus": {
-            "type": "string",
-            "enum": [
-                "DeletionRequestSent",
-                "DeletionRequestRejected",
-                "ToBeDeleted",
-                "ToBeDeletedByPeer",
-                "DeletedByPeer",
-                "DeletedByOwner"
-            ]
-        },
-        "ISO8601DateTimeString": {
-            "type": "string",
-            "errorMessage": "must match ISO8601 datetime format",
-            "pattern": "^([+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24:?00)([.,]\\d+(?!:))?)?(\\17[0-5]\\d([.,]\\d+)?)?([zZ]|([+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$"
         }
     }
 }
