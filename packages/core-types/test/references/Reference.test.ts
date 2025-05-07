128d148add263aef26058ad8e9f445e493e4a1d9
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-05-07 16:04:24 +0200

    Provide truncated url (#489)
    * feat: add toUrl to reference
    
    * feat: transport
    
    * feat: runtime
    
    * fix: make compiling again
    
    * fix: tests
    
    * chore: test failure
    
    * chore: change url
    
    * refactor: use the backbones baseUrl instead of the backbones hostname for backboneBaseUrl
    
    * refactor: always provide truncatedReference and url
    
    * refactor: undo most changes and inject config using container
    
    * chore: undo more things
    
    * chore: undo
    
    * chore: undo changes in sync everything
    
    * chore: move isOwn back
    
    * chore: move params
    
    * chore: undo stuff
    
    * chore: undo stuff
    
    * chore: undo stuff
    
    * chore: undo stuff
    
    * chore: move method
    
    * refactor: make Reference#from safe
    
    * chore: sort token mapper according to the tokenDTO
    
    * refactor: place url in a reference object
    
    * test: reference
    
    * chore: test the from method
    
    * chore: do not send default app name
    
    * chore: References -> r
    
    * fix: correct baseUrl processing
    
    * chore: use regexes
    
    * refactor: use named capturing groups
    
    * refactor: appName -> appId
    
    ---------
    
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>

---
+++ packages/core-types/test/references/Reference.test.ts
@@ -0,0 +1,101 @@
+import { CoreBuffer, CryptoEncryptionAlgorithm, CryptoSecretKey, SodiumWrapper } from "@nmshd/crypto";
+import { CoreId, Reference, SharedPasswordProtection } from "../../src";
+
+describe("Reference", () => {
+    beforeAll(async () => await SodiumWrapper.ready());
+
+    test("toUrl", () => {
+        const reference = Reference.from({
+            id: CoreId.from("ANID1234"),
+            backboneBaseUrl: "https://backbone.example.com",
+            key: CryptoSecretKey.from({
+                secretKey: CoreBuffer.from("lerJyX8ydJDEXowq2PMMntRXXA27wgHJYA_BjnFx55Y"),
+                algorithm: CryptoEncryptionAlgorithm.XCHACHA20_POLY1305
+            })
+        });
+
+        expect(reference.toUrl()).toBe("https://backbone.example.com/r/ANID1234#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfHw");
+    });
+
+    test("toUrl with custom appId", () => {
+        const reference = Reference.from({
+            id: CoreId.from("ANID1234"),
+            backboneBaseUrl: "https://backbone.example.com",
+            key: CryptoSecretKey.from({
+                secretKey: CoreBuffer.from("lerJyX8ydJDEXowq2PMMntRXXA27wgHJYA_BjnFx55Y"),
+                algorithm: CryptoEncryptionAlgorithm.XCHACHA20_POLY1305
+            })
+        });
+
+        expect(reference.toUrl("anAppName")).toBe("https://backbone.example.com/r/ANID1234?app=anAppName#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfHw");
+    });
+
+    test("toUrl with all reference fields used", () => {
+        const reference = Reference.from({
+            id: CoreId.from("ANID1234"),
+            backboneBaseUrl: "https://backbone.example.com",
+            key: CryptoSecretKey.from({
+                secretKey: CoreBuffer.from("lerJyX8ydJDEXowq2PMMntRXXA27wgHJYA_BjnFx55Y"),
+                algorithm: CryptoEncryptionAlgorithm.XCHACHA20_POLY1305
+            }),
+            forIdentityTruncated: "1234",
+            passwordProtection: SharedPasswordProtection.from({
+                passwordType: "pw",
+                salt: CoreBuffer.fromUtf8("a16byteslongsalt")
+            })
+        });
+
+        expect(reference.toUrl("anAppName")).toBe(
+            "https://backbone.example.com/r/ANID1234?app=anAppName#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfDEyMzR8cHcmWVRFMllubDBaWE5zYjI1bmMyRnNkQT09"
+        );
+    });
+
+    test("fromUrl", () => {
+        const reference = Reference.fromUrl("https://backbone.example.com/r/ANID1234#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfHw");
+
+        expect(reference).toBeInstanceOf(Reference);
+
+        expect(reference.id.toString()).toBe("ANID1234");
+        expect(reference.backboneBaseUrl).toBe("https://backbone.example.com");
+        expect(reference.key.algorithm).toBe(CryptoEncryptionAlgorithm.XCHACHA20_POLY1305);
+        expect(reference.key.secretKey.toBase64URL()).toBe("lerJyX8ydJDEXowq2PMMntRXXA27wgHJYA_BjnFx55Y");
+        expect(reference.forIdentityTruncated).toBeUndefined();
+        expect(reference.passwordProtection).toBeUndefined();
+    });
+
+    test("fromUrl with all fields used", () => {
+        const reference = Reference.fromUrl(
+            "https://backbone.example.com/r/ANID1234?app=anAppName#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfDEyMzR8cHcmWVRFMllubDBaWE5zYjI1bmMyRnNkQT09"
+        );
+
+        expect(reference).toBeInstanceOf(Reference);
+
+        expect(reference.id.toString()).toBe("ANID1234");
+        expect(reference.backboneBaseUrl).toBe("https://backbone.example.com");
+        expect(reference.key.algorithm).toBe(CryptoEncryptionAlgorithm.XCHACHA20_POLY1305);
+        expect(reference.key.secretKey.toBase64URL()).toBe("lerJyX8ydJDEXowq2PMMntRXXA27wgHJYA_BjnFx55Y");
+        expect(reference.forIdentityTruncated).toBe("1234");
+        expect(reference.passwordProtection).toBeInstanceOf(SharedPasswordProtection);
+        expect(reference.passwordProtection!.passwordType).toBe("pw");
+        expect(reference.passwordProtection!.salt.toUtf8()).toBe("a16byteslongsalt");
+    });
+
+    test("fromUrl with multiple /r/", () => {
+        const reference = Reference.fromUrl("https://backbone.example.com/r/anotherPathSegment/r/r/ANID1234#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfHw");
+
+        expect(reference).toBeInstanceOf(Reference);
+
+        expect(reference.backboneBaseUrl).toBe("https://backbone.example.com/r/anotherPathSegment/r");
+    });
+
+    test.each([
+        "https://backbone.example.com/r/ANID1234?app=anAppName#M3xsZXJKeVg4eWRKREVYb3dxMlBNTW50UlhYQTI3d2dISllBX0JqbkZ4NTVZfHw",
+        "QU5JRDEyMzRAaHR0cHM6Ly9iYWNrYm9uZS5leGFtcGxlLmNvbXwzfGxlckp5WDh5ZEpERVhvd3EyUE1NbnRSWFhBMjd3Z0hKWUFfQmpuRng1NVl8fA",
+        "nmshd://qr#QU5JRDEyMzRAaHR0cHM6Ly9iYWNrYm9uZS5leGFtcGxlLmNvbXwzfGxlckp5WDh5ZEpERVhvd3EyUE1NbnRSWFhBMjd3Z0hKWUFfQmpuRng1NVl8fA",
+        "nmshd://tr#QU5JRDEyMzRAaHR0cHM6Ly9iYWNrYm9uZS5leGFtcGxlLmNvbXwzfGxlckp5WDh5ZEpERVhvd3EyUE1NbnRSWFhBMjd3Z0hKWUFfQmpuRng1NVl8fA"
+    ])("Reference#from called with %s", (value) => {
+        const reference = Reference.from(value);
+
+        expect(reference).toBeInstanceOf(Reference);
+    });
+});
--- packages/runtime/src/types/transport/FileDTO.ts
+++ packages/runtime/src/types/transport/FileDTO.ts
@@ -12,4 +12,8 @@ export interface FileDTO {
     title: string;
     description?: string;
     truncatedReference: string;
+    reference: {
+        truncated: string;
+        url: string;
+    };
 }
