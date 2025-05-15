64b3020451b9e53be20c97fc276cd1db988e6965
mkuhn <magnus.kuhn@js-soft.com>
2025-05-15 11:22:12 +0200

    feat: validate mail recipients

--- packages/runtime/src/useCases/transport/messages/SendMessage.ts
+++ packages/runtime/src/useCases/transport/messages/SendMessage.ts
@@ -77,6 +77,9 @@ export class SendMessageUseCase extends UseCase<SendMessageRequest, MessageDTO> 
         const recipientsValidationError = await this.validateMessageRecipients(transformedContent, recipients);
         if (recipientsValidationError) return recipientsValidationError;
 
+        const mailRecipientsValidationError = this.validateMailRecipients(transformedContent, recipients);
+        if (mailRecipientsValidationError) return mailRecipientsValidationError;
+
         if (transformedContent instanceof Request) {
             const requestValidationError = await this.validateRequestAsMessageContent(transformedContent, CoreAddress.from(recipients[0]));
             if (requestValidationError) return requestValidationError;
@@ -129,6 +132,27 @@ export class SendMessageUseCase extends UseCase<SendMessageRequest, MessageDTO> 
         }
 
         if (deletedPeers.length > 0) return TransportCoreErrors.messages.peerIsDeleted(deletedPeers);
+
+        return;
+    }
+
+    private validateMailRecipients(content: Serializable, recipients: string[]): ApplicationError | undefined {
+        if (!(content instanceof Mail)) return;
+
+        const ccRecipients = content.cc?.map((address) => address.toString()) ?? [];
+        const toRecipients = content.to.map((address) => address.toString());
+        const duplicatesInToAndCc = _.intersection(ccRecipients, toRecipients);
+        if (duplicatesInToAndCc.length > 0) {
+            return RuntimeErrors.general.invalidPropertyValue(`The recipients '${duplicatesInToAndCc.join("', '")}' are put into both 'to' and 'cc'.`);
+        }
+
+        const mailRecipients = _.union(ccRecipients, toRecipients);
+        const mismatchBetweenMailAndMessageRecipients = _.xor(recipients, mailRecipients);
+        if (mismatchBetweenMailAndMessageRecipients.length > 0) {
+            return RuntimeErrors.general.invalidPropertyValue(
+                `The identities '${mismatchBetweenMailAndMessageRecipients.join("', '")}' are not listed among both the Message recipients and the recipients in 'to'/'cc'.`
+            );
+        }
 
         return;
     }
