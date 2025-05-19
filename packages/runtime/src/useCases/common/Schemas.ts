31d177c47becf8c219f5f73f2bf5e85025aa3838
Britta Stallknecht <britta.stallknecht@js-soft.com>
2025-05-19 12:01:25 +0200

    chore: update Schema.ts

--- packages/runtime/src/useCases/common/Schemas.ts
+++ packages/runtime/src/useCases/common/Schemas.ts
@@ -23080,6 +23080,19 @@ export const GetTokensRequest: any = {
         "GetTokensQuery": {
             "type": "object",
             "properties": {
+                "isOwn": {
+                    "anyOf": [
+                        {
+                            "type": "string"
+                        },
+                        {
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            }
+                        }
+                    ]
+                },
                 "createdAt": {
                     "anyOf": [
                         {
