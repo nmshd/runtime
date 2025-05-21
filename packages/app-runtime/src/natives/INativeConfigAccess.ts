8d8266b50df0cc5892726dd0e8ec0b6c28dc222e
Julian König <julian.koenig@js-soft.com>
2025-05-21 09:14:48 +0200

    chore: remove INativeConfigAccess

--- packages/app-runtime/src/natives/INativeConfigAccess.ts
+++
@@ -1,8 +0,0 @@
-import { Result } from "@js-soft/ts-utils";
-
-export interface INativeConfigAccess {
-    get(key: string): Result<any>;
-    set(key: string, value: any): Result<void>;
-    remove(key: string): Result<void>;
-    save(): Promise<Result<void>>;
-}
