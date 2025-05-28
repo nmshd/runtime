5c494a445bbbd04891d69aa27a0daf79549a6215
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-05-28 13:03:30 +0200

    refactor: naming

--- packages/transport/src/events/FileOwnershipIsLockedEvent.ts
+++ packages/transport/src/events/FileOwnershipIsLockedEvent.ts
@@ -1,14 +1,14 @@
 import { CoreId } from "@nmshd/core-types";
 import { TransportDataEvent } from "./TransportDataEvent";
 
-export interface FileIsLockedEventEventData {
+export interface FileOwnershipIsLockedEventData {
     fileId: CoreId;
 }
 
-export class FileOwnershipIsLockedEvent extends TransportDataEvent<FileIsLockedEventEventData> {
+export class FileOwnershipIsLockedEvent extends TransportDataEvent<FileOwnershipIsLockedEventData> {
     public static readonly namespace = "transport.fileOwnershipIsLocked";
 
-    public constructor(eventTargetAddress: string, data: FileIsLockedEventEventData) {
+    public constructor(eventTargetAddress: string, data: FileOwnershipIsLockedEventData) {
         super(FileOwnershipIsLockedEvent.namespace, eventTargetAddress, data);
     }
 }
