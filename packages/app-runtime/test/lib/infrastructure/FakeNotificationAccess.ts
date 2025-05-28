1c7b0f56ea42c6230db01447ef697c5db267de09
Julian König <julian.koenig@js-soft.com>
2025-05-28 13:52:42 +0200

    chore: reword log messages

--- packages/app-runtime/test/lib/infrastructure/FakeNotificationAccess.ts
+++ packages/app-runtime/test/lib/infrastructure/FakeNotificationAccess.ts
@@ -6,38 +6,38 @@ export class FakeNotificationAccess implements INotificationAccess {
     public constructor(private readonly logger: ILogger) {}
 
     public getPushToken(): Promise<Result<string>> {
-        this.logger.info("NativeNotificationAccess.getPushToken()");
+        this.logger.info("FakeNotificationAccess.getPushToken()");
         return Promise.resolve(Result.ok("fake-push-token"));
     }
 
     public schedule(title: string, body: string, options?: INotificationScheduleOptions): Promise<Result<number>> {
-        this.logger.info(`NativeNotificationAccess.schedule(${title},${body},${options})`);
+        this.logger.info(`FakeNotificationAccess.schedule(${title},${body},${options})`);
 
         return Promise.resolve(Result.ok(0));
     }
 
     public update(id: number, title: string, body: string, options?: INotificationScheduleOptions): Promise<Result<void>> {
-        this.logger.info(`NativeNotificationAccess.update(${id},${title},${body},${options})`);
+        this.logger.info(`FakeNotificationAccess.update(${id},${title},${body},${options})`);
         return Promise.resolve(Result.ok(undefined));
     }
 
     public clear(id: number): Promise<Result<void>> {
-        this.logger.info(`NativeNotificationAccess.clear(${id})`);
+        this.logger.info(`FakeNotificationAccess.clear(${id})`);
         return Promise.resolve(Result.ok(undefined));
     }
 
     public clearAll(): Promise<Result<void>> {
-        this.logger.info("NativeNotificationAccess.clearAll()");
+        this.logger.info("FakeNotificationAccess.clearAll()");
         return Promise.resolve(Result.ok(undefined));
     }
 
     public getAll(): Promise<Result<number[]>> {
-        this.logger.info("NativeNotificationAccess.getAll()");
+        this.logger.info("FakeNotificationAccess.getAll()");
         return Promise.resolve(Result.ok([0]));
     }
 
     public init(): Promise<Result<void>> {
-        this.logger.info("NativeNotificationAccess.init()");
+        this.logger.info("FakeNotificationAccess.init()");
         return Promise.resolve(Result.ok(undefined));
     }
 }
