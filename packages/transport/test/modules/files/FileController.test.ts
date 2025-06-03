637e4ab7152dcf82a10df4599798234fc26e5017
Julian König <julian.koenig@js-soft.com>
2025-06-03 13:44:13 +0200

    fix: remove strange test

--- packages/transport/test/modules/files/FileController.test.ts
+++ packages/transport/test/modules/files/FileController.test.ts
@@ -223,17 +223,6 @@ describe("FileController", function () {
             expect(updatedFile.ownershipToken).not.toBe(file.ownershipToken);
         });
 
-        test("should claim the ownership of a File with a valid ownershipToken as the owner after loading it", async function () {
-            const file = await TestUtil.uploadFile(sender, CoreBuffer.fromUtf8("Test"));
-
-            const updatedFile = await sender.files.claimFileOwnership(file.id, file.ownershipToken!);
-            expect(updatedFile.cache!.owner).toStrictEqual(sender.identity.address);
-            expect(updatedFile.isOwn).toBe(true);
-
-            expect(updatedFile.ownershipToken).toBeDefined();
-            expect(updatedFile.ownershipToken).not.toBe(file.ownershipToken);
-        });
-
         test("should not claim the ownership of a File with an invalid ownershipToken", async function () {
             const file = await TestUtil.uploadFile(sender, CoreBuffer.fromUtf8("Test"));
 
