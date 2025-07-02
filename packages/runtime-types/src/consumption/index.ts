b84c976cc8489a68570a2020780253460ff18244
Julian König <julian.koenig@js-soft.com>
2025-07-02 16:12:37 +0200

    chore: remove from runtime-types

--- packages/runtime-types/src/consumption/index.ts
+++ packages/runtime-types/src/consumption/index.ts
@@ -2,7 +2,6 @@ export * from "./AttributeTagCollectionDTO";
 export * from "./DraftDTO";
 export * from "./IdentityMetadataDTO";
 export * from "./LocalAttributeDTO";
-export * from "./LocalAttributeListenerDTO";
 export * from "./LocalNotificationDTO";
 export * from "./LocalRequestDTO";
 export * from "./RequestValidationResultDTO";
