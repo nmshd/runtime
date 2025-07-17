2b7a0fb98f952b1ff3a077a25c11fa7f2a1fae07
Julian König <julian.koenig@js-soft.com>
2025-07-17 14:38:33 +0200

    fix: undumbyfy test

--- packages/transport/test/modules/messages/MessageController.test.ts
+++ packages/transport/test/modules/messages/MessageController.test.ts
@@ -268,7 +268,13 @@ describe("MessageController", function () {
     });
 
     describe("Sending Messages for terminated Relationships", function () {
+        let messageId: CoreId;
+
         beforeAll(async function () {
+            const message = await TestUtil.sendMessage(sender, recipient);
+            await TestUtil.syncUntilHasMessage(recipient, messageId);
+            messageId = message.id;
+
             await TestUtil.terminateRelationship(sender, recipient);
         });
 
@@ -277,11 +283,8 @@ describe("MessageController", function () {
         });
 
         test("should decrypt a Message on a terminated Relationship", async function () {
-            const messageId = (await TestUtil.sendMessage(sender, recipient)).id;
             await expect(sender.messages.updateBackboneData([messageId.toString()])).resolves.not.toThrow();
-
-            // TODO: what was the intention of this part of the test? Looks like it did exactly nothing
-            // await expect(recipient.messages.updateBackboneData([messageId.toString()])).resolves.not.toThrow();
+            await expect(recipient.messages.updateBackboneData([messageId.toString()])).resolves.not.toThrow();
         });
 
         test("should be able to receive a Message sent on a terminated Relationship after the Relationship was reactivated", async function () {
