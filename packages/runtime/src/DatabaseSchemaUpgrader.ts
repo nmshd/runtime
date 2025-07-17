113ada4d79ff5ededb770e0fd7406969b884958e
Julian König <julian.koenig@js-soft.com>
2025-07-17 17:33:58 +0200

    chore: remove not needed db schema upgrade

--- packages/runtime/src/DatabaseSchemaUpgrader.ts
+++ packages/runtime/src/DatabaseSchemaUpgrader.ts
@@ -1,9 +1,7 @@
-import { IDatabaseCollection } from "@js-soft/docdb-access-abstractions";
 import { ILogger, ILoggerFactory } from "@js-soft/logging-abstractions";
 import { Serializable, serialize, type, validate } from "@js-soft/ts-serval";
-import { ConsumptionController, LocalAttribute, LocalRequest } from "@nmshd/consumption";
-import { AccountController, DatawalletModification, DatawalletModificationCategory, DatawalletModificationType, TransportIds } from "@nmshd/transport";
-import _ from "lodash";
+import { ConsumptionController } from "@nmshd/consumption";
+import { AccountController } from "@nmshd/transport";
 
 @type("RuntimeDatabaseSchemaMetadata")
 class RuntimeDatabaseSchemaMetadata extends Serializable {
@@ -28,7 +26,7 @@ class RuntimeDatabaseSchemaMetadata extends Serializable {
 }
 
 export class DatabaseSchemaUpgrader {
-    private readonly CURRENT_DATABASE_SCHEMA_VERSION = 2;
+    private readonly CURRENT_DATABASE_SCHEMA_VERSION = 1;
     private readonly DATABASE_SCHEMA_QUERY = { id: RuntimeDatabaseSchemaMetadata.DATABASE_SCHEMA_ID };
 
     public constructor(
@@ -81,103 +79,5 @@ export const UPGRADE_LOGIC = Object.freeze<Record<number, UpgradeLogicFunction |
     // eslint-disable-next-line @typescript-eslint/naming-convention
     1: (accountController, _, logger) => {
         logger.info(`Upgrading database schema to version 1 for account '${accountController.identity.address.toString()}'`);
-    },
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    2: async (accountController, _, logger) => {
-        logger.info(`Upgrading database schema to version 2 for account '${accountController.identity.address.toString()}'`);
-
-        if (!accountController.config.datawalletEnabled) return;
-
-        const datawalletModifications = accountController["unpushedDatawalletModifications"] as IDatabaseCollection;
-        const datawalletVersion = accountController.config.supportedDatawalletVersion;
-
-        const requestsCollection = await accountController.getSynchronizedCollection("Requests");
-        const requestDocs = await requestsCollection.find({});
-
-        for (const requestDoc of requestDocs) {
-            logger.info(`Processing Request '${requestDoc.id}'`);
-
-            let request: LocalRequest;
-
-            try {
-                request = LocalRequest.from(requestDoc);
-            } catch (e) {
-                logger.error(`Failed to parse Request '${requestDoc.id}'`, e);
-                continue;
-            }
-
-            const objectIdentifier = request.id;
-
-            await datawalletModifications.create(
-                DatawalletModification.from({
-                    localId: await TransportIds.datawalletModification.generate(),
-                    type: DatawalletModificationType.Create,
-                    collection: "Requests",
-                    objectIdentifier,
-                    payloadCategory: DatawalletModificationCategory.TechnicalData,
-                    payload: extractPayloadFromObject(request, request.technicalProperties),
-                    datawalletVersion
-                })
-            );
-
-            await datawalletModifications.create(
-                DatawalletModification.from({
-                    localId: await TransportIds.datawalletModification.generate(),
-                    type: DatawalletModificationType.Create,
-                    collection: "Requests",
-                    objectIdentifier,
-                    payloadCategory: DatawalletModificationCategory.Userdata,
-                    payload: extractPayloadFromObject(request, request.userdataProperties),
-                    datawalletVersion
-                })
-            );
-
-            logger.info(`Successfully created datawallet modifications for Request '${requestDoc.id}'.`);
-        }
-
-        const attributesCollection = await accountController.getSynchronizedCollection("Attributes");
-        const attributeDocs = await attributesCollection.find({});
-
-        for (const attributeDoc of attributeDocs) {
-            logger.info(`Processing Attribute '${attributeDoc.id}'`);
-
-            let attribute: LocalAttribute;
-
-            try {
-                attribute = LocalAttribute.from(attributeDoc);
-            } catch (e) {
-                logger.error(`Failed to parse Attribute '${attributeDoc.id}'`, e);
-                continue;
-            }
-
-            const technicalModificationPayload = extractPayloadFromObject(attribute, attribute.technicalProperties);
-            if (!("succeededBy" in technicalModificationPayload) && !("shareInfo" in technicalModificationPayload) && !("parentId" in technicalModificationPayload)) {
-                logger.info(`Attribute '${attributeDoc.id}' does not contain any new technical properties. Skipping.`);
-                continue;
-            }
-
-            await datawalletModifications.create(
-                DatawalletModification.from({
-                    localId: await TransportIds.datawalletModification.generate(),
-                    type: DatawalletModificationType.Update,
-                    collection: "Attributes",
-                    objectIdentifier: attribute.id,
-                    payloadCategory: DatawalletModificationCategory.TechnicalData,
-                    payload: technicalModificationPayload,
-                    datawalletVersion
-                })
-            );
-
-            logger.info(`Successfully created a datawallet modification for Attribute '${attributeDoc.id}'.`);
-        }
-
-        await accountController.syncDatawallet();
     }
 });
-
-function extractPayloadFromObject(serializableObject: Serializable, properties: string[]): any {
-    const object = serializableObject.toJSON();
-    const predicate = (value: any, key: string) => value !== undefined && properties.includes(key);
-
-    return _.pickBy<any>(object, predicate);
-}
