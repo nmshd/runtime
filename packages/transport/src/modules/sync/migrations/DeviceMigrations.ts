109cfc716e0768d69661cbb4276f05a2b8693ca0
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-07-18 08:30:37 +0200

    Remove device and database migrations (#667)
    * chore: remove unneeded device migrations
    
    * chore: remove not needed db schema upgrade
    
    * chore: throw for to big db schema version
    
    * chore: rm test
    
    * chore: update comment
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/transport/src/modules/sync/migrations/DeviceMigrations.ts
+++ packages/transport/src/modules/sync/migrations/DeviceMigrations.ts
@@ -1,26 +1,10 @@
-import { CoreSynchronizable } from "../../../core/CoreSynchronizable";
 import { AccountController } from "../../accounts/AccountController";
 
 export class DeviceMigrations {
     public constructor(private readonly accountController: AccountController) {}
 
-    public async v1(): Promise<void> {
-        const query = { cache: { $exists: false } };
-        const synchronizableToId = (c: CoreSynchronizable) => c.id.toString();
-
-        const files = await this.accountController.files.getFiles(query);
-        await this.accountController.files.updateCache(files.map(synchronizableToId));
-
-        const messages = await this.accountController.messages.getMessages(query);
-        await this.accountController.messages.updateCache(messages.map(synchronizableToId));
-
-        const relationships = await this.accountController.relationships.getRelationships(query);
-        await this.accountController.relationships.updateCache(relationships.map(synchronizableToId));
-
-        const templates = await this.accountController.relationshipTemplates.getRelationshipTemplates(query);
-        await this.accountController.relationshipTemplates.updateCache(templates.map(synchronizableToId));
-
-        const tokens = await this.accountController.tokens.getTokens(query);
-        await this.accountController.tokens.updateCache(tokens.map(synchronizableToId));
+    public v1(): Promise<void> {
+        // no upgrade steps necessary for v1
+        return Promise.resolve();
     }
 }
