80eceead45b00ad2c1604555feabfc4377250fea
Julian König <julian.koenig@js-soft.com>
2025-07-24 15:59:00 +0200

    chore: wording

--- packages/transport/test/modules/backboneNotifications/BackboneNotificationsController.test.ts
+++ packages/transport/test/modules/backboneNotifications/BackboneNotificationsController.test.ts
@@ -2,7 +2,7 @@ import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
 import { AccountController, Transport } from "../../../src";
 import { TestUtil } from "../../testHelpers/TestUtil";
 
-describe("backboneNotificationsController", function () {
+describe("BackboneNotificationsController", function () {
     let connection: IDatabaseConnection;
 
     let transport: Transport;
