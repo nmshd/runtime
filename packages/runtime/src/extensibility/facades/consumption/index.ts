4e4ecc390ecd98d8ce1b139cf375a3433b7e39df
Jakob Erben <jakobus.erben@gmail.com>
2025-08-20 14:21:06 +0200

    feat: add openid4vc facade

--- packages/runtime/src/extensibility/facades/consumption/index.ts
+++ packages/runtime/src/extensibility/facades/consumption/index.ts
@@ -3,5 +3,6 @@ export * from "./DraftsFacade";
 export * from "./IdentityMetadataFacade";
 export * from "./IncomingRequestsFacade";
 export * from "./NotificationsFacade";
+export * from "./OpenId4VcHolderFacade";
 export * from "./OutgoingRequestsFacade";
 export * from "./SettingsFacade";
