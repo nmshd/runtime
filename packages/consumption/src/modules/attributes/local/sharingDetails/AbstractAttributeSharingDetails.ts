f9b5d8016372e56fc6133cdc401d8a9778391d0a
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-09-22 16:42:32 +0200

    refactor: use AbstractAttributeDeletionInfoJSON in AbstractAttributeSharingDetailsJSON

--- packages/consumption/src/modules/attributes/local/sharingDetails/AbstractAttributeSharingDetails.ts
+++ packages/consumption/src/modules/attributes/local/sharingDetails/AbstractAttributeSharingDetails.ts
@@ -1,11 +1,11 @@
 import { ISerializable, Serializable, serialize, validate } from "@js-soft/ts-serval";
 import { CoreAddress, CoreId, ICoreAddress, ICoreId } from "@nmshd/core-types";
-import { AbstractAttributeDeletionInfo, EmittedAttributeDeletionInfoJSON, IAbstractAttributeDeletionInfo, ReceivedAttributeDeletionInfoJSON } from "./deletionInfos";
+import { AbstractAttributeDeletionInfo, AbstractAttributeDeletionInfoJSON, IAbstractAttributeDeletionInfo } from "./deletionInfos";
 
 export interface AbstractAttributeSharingDetailsJSON {
     peer: string;
     sourceReference: string;
-    deletionInfo?: EmittedAttributeDeletionInfoJSON | ReceivedAttributeDeletionInfoJSON | ReceivedAttributeDeletionInfoJSON;
+    deletionInfo?: AbstractAttributeDeletionInfoJSON;
 }
 
 export interface IAbstractAttributeSharingDetails extends ISerializable {
