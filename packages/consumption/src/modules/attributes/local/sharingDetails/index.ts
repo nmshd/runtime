b6a706dd3f4c269baa0385b158f8e20b9e45573f
Milena Czierlinski <146972016+Milena-Czierlinski@users.noreply.github.com>
2025-09-22 17:58:19 +0200

    Move `PeerSharingDetailsJSON` to GetAttributes (#768)

--- packages/consumption/src/modules/attributes/local/sharingDetails/index.ts
+++ packages/consumption/src/modules/attributes/local/sharingDetails/index.ts
@@ -4,5 +4,4 @@ export * from "./ForwardedSharingDetails";
 export * from "./OwnRelationshipAttributeSharingDetails";
 export * from "./PeerIdentityAttributeSharingDetails";
 export * from "./PeerRelationshipAttributeSharingDetails";
-export * from "./PeerSharingDetails";
 export * from "./ThirdPartyRelationshipAttributeSharingDetails";
