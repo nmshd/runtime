776da10aaf5ede874f14e2bde583361ed5781b7f
mkuhn <magnus.kuhn@js-soft.com>
2025-10-10 09:13:11 +0200

    refactor: copy verifier module to request module

--- packages/runtime/src/modules/RequestModule.ts
+++ packages/runtime/src/modules/RequestModule.ts
@@ -1,5 +1,17 @@
 import { LocalRequestStatus } from "@nmshd/consumption";
-import { RelationshipCreationContent, RequestJSON, ResponseJSON, ResponseResult, ResponseWrapper } from "@nmshd/content";
+import {
+    DCQLQueryJSON,
+    ReadAttributeAcceptResponseItemJSON,
+    ReadAttributeRequestItemJSON,
+    RelationshipCreationContent,
+    RequestItemGroupJSON,
+    RequestJSON,
+    ResponseItemGroupJSON,
+    ResponseJSON,
+    ResponseResult,
+    ResponseWrapper,
+    VerifiableCredentialJSON
+} from "@nmshd/content";
 import { CoreDate } from "@nmshd/core-types";
 import { LocalRequestDTO, RelationshipStatus } from "@nmshd/runtime-types";
 import {
@@ -8,6 +20,7 @@ import {
     MessageProcessedResult,
     MessageReceivedEvent,
     MessageSentEvent,
+    OutgoingRequestStatusChangedEvent,
     PeerRelationshipTemplateLoadedEvent,
     RelationshipChangedEvent
 } from "../events";
@@ -26,6 +39,7 @@ export class RequestModule extends RuntimeModule {
         this.subscribeToEvent(MessageSentEvent, this.handleMessageSentEvent.bind(this));
         this.subscribeToEvent(IncomingRequestStatusChangedEvent, this.handleIncomingRequestStatusChanged.bind(this));
         this.subscribeToEvent(RelationshipChangedEvent, this.handleRelationshipChangedEvent.bind(this));
+        this.subscribeToEvent(OutgoingRequestStatusChangedEvent, this.handleOutgoingRequestStatusChanged.bind(this));
     }
 
     private async handlePeerRelationshipTemplateLoaded(event: PeerRelationshipTemplateLoadedEvent) {
@@ -369,5 +383,37 @@ export class RequestModule extends RuntimeModule {
             this.logger.error(`Could not create and complete request for templateId '${templateId}' and relationshipId '${relationship.id}'. Root error:`, result.error);
             return;
         }
+    }
+
+    private async handleOutgoingRequestStatusChanged(event: OutgoingRequestStatusChangedEvent) {
+        if (event.data.newStatus !== LocalRequestStatus.Completed) return;
+
+        const requestItems = event.data.request.content.items.map((i) => (i["@type"] === "RequestItemGroup" ? (i as RequestItemGroupJSON).items : [i])).flat();
+        const responseItems = event.data.request.response!.content.items.map((i) => (i["@type"] === "ResponseItemGroup" ? (i as ResponseItemGroupJSON).items : [i])).flat();
+        const requestResponseCombinedItems = requestItems.map((item, i) => {
+            return {
+                requestItem: item,
+                responseItem: responseItems[i]
+            };
+        });
+
+        const itemsWithSharedCredential = requestResponseCombinedItems.filter(
+            (i) => i.requestItem["@type"] === "ReadAttributeRequestItem" && (i.requestItem as ReadAttributeRequestItemJSON).query["@type"] === "DCQLQuery"
+        );
+
+        const queryPresentationCombinedItems = itemsWithSharedCredential.map((i) => {
+            return {
+                query: (i.requestItem as ReadAttributeRequestItemJSON).query as DCQLQueryJSON,
+                credential: (i.responseItem as ReadAttributeAcceptResponseItemJSON).attribute.value as VerifiableCredentialJSON
+            };
+        });
+
+        const services = await this.runtime.getServices(event.eventTargetAddress);
+        const verificationResults = await Promise.all(queryPresentationCombinedItems.map((i) => services.consumptionServices.openId4Vc.verifySharedCredential(i)));
+
+        this.logger.info(
+            `Verifying the response to request ${event.data.request.id} had the results`,
+            verificationResults.map((r) => (r.value.verified ? "Success, " : `Error ${r.value.reason}, `))
+        );
     }
 }
