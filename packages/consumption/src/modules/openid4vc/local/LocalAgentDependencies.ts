d6c48728f4259aec9f17d2754f4f6fede3182a8a
Julian König <julian.koenig@js-soft.com>
2025-11-11 12:44:49 +0100

    chore: simplify agentDependencies

--- packages/consumption/src/modules/openid4vc/local/LocalAgentDependencies.ts
+++ packages/consumption/src/modules/openid4vc/local/LocalAgentDependencies.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { AgentDependencies } from "@credo-ts/core";
 import { EventEmitter } from "events";
 import { Agent, fetch as undiciFetch } from "undici";
@@ -11,23 +10,12 @@ export const agentDependencies: AgentDependencies = {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     EventEmitterClass: EventEmitter,
     fetch: (async (input, init) => {
-        console.log("############### agentDependencies-fetch-input #################", input);
-        console.log("############### agentDependencies-fetch-init #################", init);
+        const response = await undiciFetch(input as any, {
+            ...(init as any),
+            dispatcher: new Agent({})
+        });
 
-        try {
-            const response = await undiciFetch(input as any, {
-                ...(init as any),
-                dispatcher: new Agent({ connectTimeout: 10_000, connections: 1_000, connect: { rejectUnauthorized: false } })
-            });
-
-            console.log("############### agentDependencies-fetch-response #################", response);
-
-            return response;
-        } catch (error) {
-            console.error("############### agentDependencies-fetch-error #################", error);
-            console.error("############### agentDependencies-fetch-error-stack #################", (error as any).stack);
-            throw error;
-        }
+        return response;
     }) as typeof fetch,
     // eslint-disable-next-line @typescript-eslint/naming-convention
     WebSocketClass: webSocket
