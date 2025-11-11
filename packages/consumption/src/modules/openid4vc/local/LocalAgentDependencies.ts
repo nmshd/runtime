69bf0ea1d55e2b83264ac754cfe34266afcd0cfa
Julian König <julian.koenig@js-soft.com>
2025-11-11 11:48:34 +0100

    chore: try axios

--- packages/consumption/src/modules/openid4vc/local/LocalAgentDependencies.ts
+++ packages/consumption/src/modules/openid4vc/local/LocalAgentDependencies.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { AgentDependencies } from "@credo-ts/core";
+import axios from "axios";
 import { EventEmitter } from "events";
-import { fetch as undiciFetch } from "undici";
 import webSocket from "ws";
 import { EnmeshedHolderFileSystem } from "./EnmeshedHolderFileSystem";
 
@@ -15,11 +15,18 @@ export const agentDependencies: AgentDependencies = {
         console.log("############### agentDependencies-fetch-init #################", init);
 
         try {
-            const response = await undiciFetch(input as any, init as any);
+            //     const response = await undiciFetch(input as any, init as any);
+            const response = await axios({
+                method: init?.method ?? "GET",
+                url: input.toString(),
+                headers: init?.headers as Record<string, string> | undefined,
+                data: init?.body
+            });
 
             console.log("############### agentDependencies-fetch-response #################", response);
 
-            return response;
+            // return response;
+            return new Response(response.data, { ...response } as ResponseInit);
         } catch (error) {
             console.error("############### agentDependencies-fetch-error #################", error);
             console.error("############### agentDependencies-fetch-error-stack #################", (error as any).stack);
