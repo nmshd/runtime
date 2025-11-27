5867dc09c267fef6310489affd94ca085164f517
britsta <britta.stallknecht@js-soft.com>
2025-11-27 17:29:12 +0100

    feat: add simple BIC validation for BankCode

--- packages/content/src/attributes/types/bank/BankCode.ts
+++ packages/content/src/attributes/types/bank/BankCode.ts
@@ -1,4 +1,4 @@
-import { type } from "@js-soft/ts-serval";
+import { serialize, type, validate } from "@js-soft/ts-serval";
 import { AbstractString, AbstractStringJSON, IAbstractString } from "../AbstractString";
 
 export interface BankCodeJSON extends AbstractStringJSON {
@@ -9,6 +9,16 @@ export interface IBankCode extends IAbstractString {}
 
 @type("BankCode")
 export class BankCode extends AbstractString implements IBankCode {
+    private static readonly regExp = new RegExp( /^[A-Z]{4}[A-Z]{2}[A-Z0-9]{2}([A-Z0-9]{3})?$/ );
+
+    @serialize()
+    @validate({
+        min: 8,
+        max: 11,
+        regExp: BankCode.regExp
+    })
+    public override value: string;
+
     public static from(value: IBankCode | Omit<BankCodeJSON, "@type"> | string): BankCode {
         return this.fromAny(value);
     }
