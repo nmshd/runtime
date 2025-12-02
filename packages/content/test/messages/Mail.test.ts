42487652d182f881109dd130568110f78231d542
Julian König <julian.koenig@js-soft.com>
2025-12-02 10:56:06 +0100

    test: mail bodyformat

--- packages/content/test/messages/Mail.test.ts
+++ packages/content/test/messages/Mail.test.ts
@@ -1,5 +1,5 @@
 import { Serializable } from "@js-soft/ts-serval";
-import { Mail } from "../../src";
+import { Mail, MailBodyFormat } from "../../src";
 
 describe("Mail", function () {
     test("should create a Mail from JSON", function () {
@@ -24,6 +24,15 @@ describe("Mail", function () {
 
         expect(mail).toBeInstanceOf(Mail);
     });
+
+    test.each([MailBodyFormat.PlainText, MailBodyFormat.Markdown, "PlainText", "Markdown", undefined] satisfies (MailBodyFormat | "PlainText" | "Markdown" | undefined)[])(
+        "should create a mail with different body formats",
+        function (bodyFormat) {
+            const mail = Mail.from({ to: ["did:e:a-domain:dids:anidentity"], cc: [], subject: "aSubject", body: "aBody", bodyFormat });
+            expect(mail).toBeInstanceOf(Mail);
+            expect(mail.bodyFormat).toBe(bodyFormat);
+        }
+    );
 
     test("should throw an Error if to is empty", function () {
         let error: any;
@@ -60,5 +69,25 @@ describe("Mail", function () {
 
         expect(error).toBeDefined();
         expect(error.message).toBe("Mail.to :: Value is not defined");
+    });
+
+    test("should throw an Error if an invalid bodyFormat is passed", function () {
+        let error: any;
+
+        try {
+            Serializable.fromUnknown({
+                "@type": "Mail",
+                to: ["did:e:a-domain:dids:anidentity"],
+                cc: [],
+                subject: "aSubject",
+                body: "aBody",
+                bodyFormat: "InvalidFormat"
+            });
+        } catch (e) {
+            error = e;
+        }
+
+        expect(error).toBeDefined();
+        expect(error.message).toBe("Mail.bodyFormat:String :: must be one of: PlainText,Markdown");
     });
 });
