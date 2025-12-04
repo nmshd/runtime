fd359b5cb93b276beb12ae141fd330a5b56c5328
Julian König <julian.koenig@js-soft.com>
2025-12-04 11:30:02 +0100

    feat: add ShareCredentialOfferRequestItem

---
+++ packages/content/src/requests/items/shareCredentialOffer/ShareCredentialOfferRequestItem.ts
@@ -0,0 +1,31 @@
+import { serialize, type, validate } from "@js-soft/ts-serval";
+import { RequestItemJSON } from "../..";
+import { IRequestItem, RequestItem } from "../../RequestItem";
+
+export interface ShareCredentialOfferRequestItemJSON extends RequestItemJSON {
+    "@type": "ShareCredentialOfferRequestItem";
+    credentialOfferUrl: string;
+}
+
+export interface IShareCredentialOfferRequestItem extends IRequestItem {
+    credentialOfferUrl: string;
+}
+
+@type("ShareCredentialOfferRequestItem")
+export class ShareCredentialOfferRequestItem extends RequestItem implements IShareCredentialOfferRequestItem {
+    // TODO: validate URL format?
+
+    @serialize()
+    @validate()
+    public credentialOfferUrl: string;
+
+    public static from(
+        value: IShareCredentialOfferRequestItem | Omit<ShareCredentialOfferRequestItemJSON, "@type"> | ShareCredentialOfferRequestItemJSON
+    ): ShareCredentialOfferRequestItem {
+        return this.fromAny(value);
+    }
+
+    public override toJSON(verbose?: boolean | undefined, serializeAsString?: boolean | undefined): ShareCredentialOfferRequestItemJSON {
+        return super.toJSON(verbose, serializeAsString) as ShareCredentialOfferRequestItemJSON;
+    }
+}
