10d176b6ebdd5c38d9f278a75d55b1f3463f9356
Jakob Erben <jakobus.erben@gmail.com>
2025-12-15 11:03:51 +0100

    fix: transfer token in a proper manner and only allow token or pin

--- packages/runtime/src/useCases/consumption/openid4vc/RequestCredentials.ts
+++ packages/runtime/src/useCases/consumption/openid4vc/RequestCredentials.ts
@@ -1,24 +1,24 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { OpenId4VciRequestTokenResponse, OpenId4VciResolvedCredentialOffer } from "@credo-ts/openid4vc";
+import { OpenId4VciResolvedCredentialOffer } from "@credo-ts/openid4vc";
 import { Result } from "@js-soft/ts-utils";
 import { OpenId4VcController, OpenId4VciCredentialResponseJSON } from "@nmshd/consumption";
 import { Inject } from "@nmshd/typescript-ioc";
 import { SchemaRepository, SchemaValidator, UseCase } from "../../common";
 
+export type Oid4vciAuthentication = {} | { pinCode?: string } | { accessToken?: string };
+
 export interface AbstractRequestCredentialsRequest<T> {
     credentialOffer: T;
-    pinCode?: string;
-    accessToken?: string;
     credentialConfigurationIds: string[];
+    authentication: Oid4vciAuthentication;
 }
 
 export interface RequestCredentialsResponse {
     credentialResponses: OpenId4VciCredentialResponseJSON[];
 }
 
-export interface RequestCredentialsRequest extends AbstractRequestCredentialsRequest<OpenId4VciResolvedCredentialOffer> {}
+export type RequestCredentialsRequest = AbstractRequestCredentialsRequest<OpenId4VciResolvedCredentialOffer>;
 
-export interface SchemaValidatableRequestCredentialsRequest extends AbstractRequestCredentialsRequest<Record<string, any>> {}
+export type SchemaValidatableRequestCredentialsRequest = AbstractRequestCredentialsRequest<Record<string, any>>;
 
 class Validator extends SchemaValidator<RequestCredentialsRequest> {
     public constructor(@Inject schemaRepository: SchemaRepository) {
@@ -35,20 +35,12 @@ export class RequestCredentialsUseCase extends UseCase<RequestCredentialsRequest
     }
 
     protected override async executeInternal(request: RequestCredentialsRequest): Promise<Result<RequestCredentialsResponse>> {
-        let token: OpenId4VciRequestTokenResponse | undefined;
-        if (request.accessToken !== undefined) {
-            // credo provides its own machanism to handle authentication and therefore expects the token to be in this format
-            // as we however can not use credo's solution from within the app calling this code, we need to manually create this structure here
-            token = {
-                accessToken: request.accessToken,
-                accessTokenResponse: {
-                    access_token: request.accessToken,
-                    token_type: "bearer"
-                }
-            };
-        }
-
-        const credentialResponses = await this.openId4VcController.requestCredentials(request.credentialOffer, request.credentialConfigurationIds, request.pinCode, token);
+        const credentialResponses = await this.openId4VcController.requestCredentials(
+            request.credentialOffer,
+            request.credentialConfigurationIds,
+            "pinCode" in request.authentication ? request.authentication.pinCode : undefined,
+            "accessToken" in request.authentication ? request.authentication.accessToken : undefined
+        );
         return Result.ok({ credentialResponses: credentialResponses });
     }
 }
