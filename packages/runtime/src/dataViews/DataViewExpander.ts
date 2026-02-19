d4bb2930a2c872838c97a3a5fbcdafc873fb44f1
Jakob Erben <jakobus.erben@gmail.com>
2026-02-19 12:43:25 +0100

    chore: improve error handeling

--- packages/runtime/src/dataViews/DataViewExpander.ts
+++ packages/runtime/src/dataViews/DataViewExpander.ts
@@ -2,6 +2,7 @@ import { Serializable, SerializableBase } from "@js-soft/ts-serval";
 import {
     ConsumptionController,
     LocalRequestStatus,
+    OpenId4VciCredentialResponseJSON,
     OwnIdentityAttribute,
     OwnRelationshipAttribute,
     PeerIdentityAttribute,
@@ -143,7 +144,7 @@ import {
 import { FileDVO, IdentityDVO, MessageDVO, MessageStatus, RecipientDVO, RelationshipDVO, RelationshipDirection, RelationshipTemplateDVO } from "./transport";
 
 export class DataViewExpander {
-    private readonly credentialOfferCache = new Map<string, Promise<any>>();
+    private readonly credentialOfferCache = new Map<string, Promise<OpenId4VciCredentialResponseJSON[] | undefined>>();
     public constructor(
         @Inject private readonly transport: TransportServices,
         @Inject private readonly consumption: ConsumptionServices,
@@ -648,6 +649,7 @@ export class DataViewExpander {
                         try {
                             return await this.consumptionController.openId4Vc.requestAllCredentialsFromCredentialOfferUrl(shareCredentialOfferRequestItem.credentialOfferUrl);
                         } catch {
+                            this.credentialOfferCache.delete(shareCredentialOfferRequestItem.credentialOfferUrl);
                             return undefined;
                         }
                     })();
@@ -656,12 +658,16 @@ export class DataViewExpander {
                 }
                 const credentialResponses = await credentialResponsesPromise;
 
+                if (credentialResponses === undefined) {
+                    throw new Error("A credential offer could not be retrieved.");
+                }
+
                 return {
                     ...shareCredentialOfferRequestItem,
                     type: "ShareCredentialOfferRequestItemDVO",
                     id: "",
                     name: this.generateRequestItemName(requestItem["@type"], isDecidable),
-                    isDecidable: isDecidable && !!credentialResponses,
+                    isDecidable: isDecidable && credentialResponses.length > 0,
                     response: responseItemDVO,
                     credentialResponses
                 } as ShareCredentialOfferRequestItemDVO;
