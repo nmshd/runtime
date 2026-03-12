bbdc007b516522070a001236b52d4e2adb3ef933
Milena Czierlinski <146972016+Milena-Czierlinski@users.noreply.github.com>
2026-03-12 15:40:25 +0100

    Specify `expiresAt` and `ephemeral` when creating a PresentationToken (#994)
    * feat: expose expiresAt and ephemeral when creating a presentation token
    
    * refactor: specify type of input string
    
    * chore: rebuild schemas

--- packages/runtime/src/useCases/common/Schemas.ts
+++ packages/runtime/src/useCases/common/Schemas.ts
@@ -17337,12 +17337,25 @@ export const CreatePresentationTokenRequest: any = {
             "properties": {
                 "attributeId": {
                     "type": "string"
+                },
+                "expiresAt": {
+                    "$ref": "#/definitions/ISO8601DateTimeString"
+                },
+                "ephemeral": {
+                    "type": "boolean"
                 }
             },
             "required": [
-                "attributeId"
+                "attributeId",
+                "expiresAt",
+                "ephemeral"
             ],
             "additionalProperties": false
+        },
+        "ISO8601DateTimeString": {
+            "type": "string",
+            "errorMessage": "must match ISO8601 datetime format",
+            "pattern": "^([+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24:?00)([.,]\\d+(?!:))?)?(\\17[0-5]\\d([.,]\\d+)?)?([zZ]|([+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$"
         }
     }
 }
