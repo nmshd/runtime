e5034fc893271a180d17fe675f9e3da75085fdbe
Milena-Czierlinski <milena.czierlinski@js-soft.com>
2026-04-17 09:14:35 +0200

    feat: handle type error

--- packages/content/src/attributes/types/utils/validateJSON.ts
+++ packages/content/src/attributes/types/utils/validateJSON.ts
@@ -1,13 +1,10 @@
 export function validateJSON(value: any): string | undefined {
     try {
         const string = JSON.stringify(value);
-        if (string.length > 4096) {
-            return "stringified value must not be longer than 4096 characters";
-        }
+
+        if (string.length > 4096) return "stringified value must not be longer than 4096 characters";
     } catch (e) {
-        if (e instanceof SyntaxError) {
-            return "must be a valid JSON object";
-        }
+        if (e instanceof SyntaxError || e instanceof TypeError) return "must be a valid JSON object";
 
         return "could not validate value";
     }
