55705c66bf1253fae8a13f34ef613262531e88b9
Milena Czierlinski <146972016+Milena-Czierlinski@users.noreply.github.com>
2026-08-20 15:25:17 +0200

    Cached VC images are not saved when storing a credential- #1149 (#1149)

--- packages/consumption/src/modules/openid4vc/local/OpenId4VciCredentialResponseJSON.ts
+++ packages/consumption/src/modules/openid4vc/local/OpenId4VciCredentialResponseJSON.ts
@@ -1,7 +1,9 @@
 import { ClaimFormat, W3cJsonCredential } from "@credo-ts/core";
+import { DisplayInformationCachedImagesJSON } from "@nmshd/content";
 
 export interface OpenId4VciCredentialResponseJSON {
     claimFormat: ClaimFormat;
     encoded: string | W3cJsonCredential;
     displayInformation?: Record<string, any>[];
+    displayInformationCachedImages?: DisplayInformationCachedImagesJSON[];
 }
